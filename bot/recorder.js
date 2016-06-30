6a4deba0cbaf957f51c9de10ee942fc2fff83112
Carlos Rodriguez <carlos@s8f.org>
2016-06-29 17:03:29 -0700

    tweet support for recorder

--- bot/recorder.js
+++ bot/recorder.js
@@ -5,6 +5,17 @@ module.exports = function container (get, set, clear) {
     var counter = 0
     var lastTick = new Date().getTime()
 
+    if (options.tweet) {
+      var twitterClient = get('utils.twitterClient')
+      function onTweet (err, data, response) {
+        if (err) return get('console').error('tweet err', err)
+        if (response.statusCode === 200 && data && data.id_str) {
+          get('console').log('tweeted: '.cyan + data.text.white)
+        }
+        else get('console').error('tweet err', response.statusCode, data)
+      }
+    }
+
     function onTick () {
       var trade_ticker = ''
       var params = {
@@ -20,8 +31,14 @@ module.exports = function container (get, set, clear) {
       lastTick = new Date().getTime()
       get('db.trades').select(params, function (err, trades) {
         if (err) return get('console').error('trade select err', err)
-        var ticker = get('db.ticks').create(trades)
-        get('console').log('saw ' + counter + ' messages.' + ticker)
+        var tick = get('db.ticks').create(trades)
+        get('console').log('saw ' + counter + ' messages.' + (tick ? tick.trade_ticker : ''))
+        if (tick && options.tweet && tick.vol > 20) {
+          var tweet = {
+            status: 'big trade: ' + tick.ticker + ' BTC at ' + get('utils.getTimestamp')(tick.time)
+          }
+          twitterClient.post('statuses/update', tweet, onTweet)
+        }
         if (counter === 0) {
           get('console').log('no messages in last tick. rebooting socket...')
           reboot()
