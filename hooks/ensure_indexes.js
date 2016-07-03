259a9fd19486f180227d295a82caf1621a1e1356
Carlos Rodriguez <carlos@s8f.org>
2016-07-03 03:55:49 -0700

    get rid of -1 index

--- hooks/ensure_indexes.js
+++ hooks/ensure_indexes.js
@@ -2,13 +2,7 @@ module.exports = function container (get, set) {
   return function ensure_indexes (cb) {
     var tasks = []
     tasks.push(function (done) {
-      get('db.mongo.db').collection('trades').ensureIndex({time: -1}, done)
-    })
-    tasks.push(function (done) {
       get('db.mongo.db').collection('trades').ensureIndex({time: 1}, done)
-    })
-    tasks.push(function (done) {
-      get('db.mongo.db').collection('ticks').ensureIndex({time: -1}, done)
     })
     tasks.push(function (done) {
       get('db.mongo.db').collection('ticks').ensureIndex({time: 1}, done)
