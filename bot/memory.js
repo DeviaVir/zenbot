a64a341c97e291c8307421d357ba927a5b862bb1
Carlos Rodriguez <carlos@s8f.org>
2016-07-04 14:14:43 -0700

    more mem output

--- bot/memory.js
+++ bot/memory.js
@@ -1,9 +1,14 @@
+var constants = require('../conf/constants.json')
+
 module.exports = function container (get, set, clear) {
   var bot = get('bot')
-  get('db.mems').load('learned', function (err, learned) {
+  get('db.mems').load(constants.product_id, function (err, rs) {
     if (err) throw err
-    console.log(JSON.stringify(learned, null, 2))
-    setTimeout(process.exit, 1000)
+    get('db.mems').load('learned', function (err, learned) {
+      if (err) throw err
+      console.log(JSON.stringify({rs: rs, learned: learned}, null, 2))
+      setTimeout(process.exit, 1000)
+    })
   })
   return null
 }
