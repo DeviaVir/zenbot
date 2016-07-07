c44f702553aeab822b209b0acbe80ed4598415fe
Carlos Rodriguez <carlos@s8f.org>
2016-07-06 19:47:38 -0700

    no longer need trades coll

--- db/_codemap.js
+++ db/_codemap.js
@@ -4,10 +4,8 @@ module.exports = {
   _folder: 'db',
   'mems': require('./mems'),
   'ticks': require('./ticks'),
-  'trades': require('./trades'),
   'collections[]': [
     '#db.mems',
-    '#db.ticks',
-    '#db.trades'
+    '#db.ticks'
   ]
 }
--- hooks/ensure_indexes.js
+++ hooks/ensure_indexes.js
@@ -2,9 +2,6 @@ module.exports = function container (get, set) {
   return function ensure_indexes (cb) {
     var tasks = []
     tasks.push(function (done) {
-      get('db.mongo.db').collection('trades').ensureIndex({time: 1}, done)
-    })
-    tasks.push(function (done) {
       get('db.mongo.db').collection('ticks').ensureIndex({time: 1}, done)
     })
     get('vendor.run-series')(tasks, cb)
