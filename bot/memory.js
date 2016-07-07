b0d6f5e401138506312816a90ef43400ed6e0ace
Carlos Rodriguez <carlos@s8f.org>
2016-07-06 21:03:48 -0700

    add tick timestamps to mem

--- bot/memory.js
+++ bot/memory.js
@@ -2,12 +2,34 @@ var constants = require('../conf/constants.json')
 
 module.exports = function container (get, set, clear) {
   var bot = get('bot')
+  var resp = {}
+  var get_timestamp = get('utils.get_timestamp')
   get('db.mems').load(constants.product_id, function (err, rs) {
     if (err) throw err
+    resp.rs = rs
     get('db.mems').load('learned', function (err, learned) {
       if (err) throw err
-      console.log(JSON.stringify({rs: rs, learned: learned}, null, 2))
-      setTimeout(process.exit, 1000)
+      resp.learned = learned
+      get('db.ticks').select({limit: 1, sort: {time: 1}}, function (err, ticks) {
+        if (err) throw err
+        if (ticks.length) {
+          resp.first_timestamp = get_timestamp(ticks[0].time)
+        }
+        else {
+          resp.first_timestamp = null
+        }
+         get('db.ticks').select({limit: 1, sort: {time: -1}}, function (err, ticks) {
+          if (err) throw err
+          if (ticks.length) {
+            resp.last_timestamp = get_timestamp(ticks[0].time)
+          }
+          else {
+            resp.last_timestamp = null
+          }
+          console.log(JSON.stringify(resp, null, 2))
+          setTimeout(process.exit, 1000)
+        })
+      })
     })
   })
   return null
