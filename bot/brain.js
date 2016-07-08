a25ef259eec79d10d15ccb9c69ae97e9dcb92bf9
Carlos Rodriguez <carlos@s8f.org>
2016-07-08 13:10:33 -0700

    fix

--- bot/brain.js
+++ bot/brain.js
@@ -224,6 +224,7 @@ module.exports = function container (get, set, clear) {
         .value()
       if (rs.vol < 0) rs.side = tick.side
       rs.vol = Math.abs(rs.vol)
+      rs.max_vol = 0
     }
     else {
       rs.side = tick.side
@@ -243,6 +244,7 @@ module.exports = function container (get, set, clear) {
     if (rs.vol >= bot.min_vol) {
       var trigger_vol = rs.vol
       rs.vol = 0
+      rs.max_vol = 0
       // trigger
       if (rs.cooldown >= 1) rs.cooldown--
       else rs.cooldown = 0
