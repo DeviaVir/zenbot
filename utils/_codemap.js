34f29954882e57c52bd22239c51b0608a2530dd5
Carlos Rodriguez <carlos@s8f.org>
2016-07-11 02:15:11 -0700

    fix

--- utils/_codemap.js
+++ utils/_codemap.js
@@ -4,7 +4,6 @@ module.exports = {
   'authed_client': require('./authed_client'),
   'client': require('./client'),
   '@motley:console': require('./console'),
-  'filter_logs': require('./filter_logs'),
   'get_time': require('./get_time'),
   'get_timestamp': require('./get_timestamp'),
   'twitter_client': require('./twitter_client'),
