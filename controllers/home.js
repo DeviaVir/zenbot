e9db3b54926cf5244b53197a5569ed564e8b1d38
Carlos Rodriguez <carlos@s8f.org>
2016-07-11 12:53:19 -0700

    fix

--- controllers/home.js
+++ controllers/home.js
@@ -3,25 +3,7 @@ var constants = require('../conf/constants.json')
 module.exports = function container (get, set) {
   return get('controller')()
     .get('/', function (req, res, next) {
-      var params = {
-        limit: constants.log_limit,
-        sort: {time: -1},
-        query: {
-          public: !res.vars.secret
-        }
-      }
-      get('db.logs').select(params, function (err, logs) {
-        if (err) return next(err)
-        var zmi
-        logs.forEach(function (log) {
-          if (zmi) return
-          if (log.data && log.data.zmi) zmi = log.data.zmi
-        })
-        if (zmi) {
-          res.vars.title = zmi + ' - ' + res.vars.title
-        }
-        res.render('home')
-      })
+      res.render('home')
     })
     .get('/forget', function (req, res, next) {
       delete req.session.secret
--- hooks/_codemap.js
+++ hooks/_codemap.js
@@ -4,10 +4,6 @@ module.exports = {
   _folder: 'hooks',
 
   'ensure_indexes': require('./ensure_indexes'),
-  'register_helpers': require('./register_helpers'),
-  'boot[]': [
-    '#hooks.register_helpers'
-  ],
   'mount[]': [
     '#hooks.ensure_indexes'
   ]
