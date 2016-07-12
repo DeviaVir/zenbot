040963fba3fa36998671b9ab6d10dbf40887a3c7
Carlos Rodriguez <carlos@s8f.org>
2016-07-11 19:34:20 -0700

    linkify

--- db/logs.js
+++ db/logs.js
@@ -4,7 +4,7 @@ module.exports = function container (get, set) {
   return get('db.createCollection')('logs', {
     load: function (obj, opts, cb) {
       // respond after the obj is loaded
-      obj.html = ansi_up.ansi_to_html(ansi_up.escape_for_html(obj.line), {use_classes: true})
+      obj.html = ansi_up.linkify(ansi_up.ansi_to_html(ansi_up.escape_for_html(obj.line), {use_classes: true}))
       cb(null, obj);
     },
     save: function (obj, opts, cb) {
