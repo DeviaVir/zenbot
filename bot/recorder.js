ef0a1d6338e12a58ad42ea0b6fc02d8f5c7881a5
Carlos Rodriguez <carlos@s8f.org>
2016-07-14 12:27:27 -0700

    sort by volume

--- bot/recorder.js
+++ bot/recorder.js
@@ -33,6 +33,11 @@ module.exports = function container (get, set, clear) {
             }
             if (results.length) {
               var max_time = 0
+              results.sort(function (a, b) {
+                if (a.size > b.size) return -1
+                if (a.size < b.size) return 1
+                return 0
+              })
               var ticker = results.slice(0, 3).map(function (trade) {
                 max_time = Math.max(max_time, trade.time)
                 return trade.side + ' ' + n(trade.size).format('0.000') + ' ' + trade.asset + ' at ' + n(trade.price).format('0.000') + ' ' + trade.currency
