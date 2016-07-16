08d09fceea108090f0194965e8908b1f26faa989
Carlos Rodriguez <carlos@s8f.org>
2016-07-15 19:25:15 -0700

    colors

--- utils/log_trades.js
+++ utils/log_trades.js
@@ -15,7 +15,7 @@ module.exports = function container (get, set, clear) {
     var min_time
     var ticker = trades.slice(0, c.log_trades_limit).map(function (trade) {
       min_time = min_time ? Math.min(min_time, trade.time) : trade.time
-      return z(4, trade.side, ' ') + ' ' + z(9, n(trade.size).format('0.000'), ' ').white + ' ' + config.asset + ' at '.grey + z(9, n(trade.price).format('0.000'), ' ').yellow + ' ' + config.currency.yellow
+      return z(4, (trade.side === 'buy' ? 'SELL' : 'BUY') , ' ')[trade.side === 'buy' ? 'red' : 'green'] + ' ' + z(9, n(trade.size).format('0.000'), ' ')[trade.side === 'buy' ? 'red' : 'green'] + ' ' + config.asset + ' at '.grey + z(9, n(trade.price).format('0.000'), ' ').yellow + ' ' + config.currency.grey
     }).join(', ')
     ticker = get_timestamp(min_time).grey + ' ' + ticker
     get('logger').info(('[' + exchange.slug + ']').cyan + ' processed ' + z(5, trades.length, ' ') + ' trades. ' + ticker)
