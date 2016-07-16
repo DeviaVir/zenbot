7175cf40462633755f2e9cf76a37a9edb33e823a
Carlos Rodriguez <carlos@s8f.org>
2016-07-15 17:17:15 -0700

    logging

--- plugins/default_backfiller/backfill_command.js
+++ plugins/default_backfiller/backfill_command.js
@@ -2,12 +2,15 @@ module.exports = function container (get, set, clear) {
   var program = get('program')
   var launcher = get('launcher')
   var backfiller = get('backfiller')
-  return function () {
-    program
-      .command('backfill')
-      .description('2. run the backfiller')
-      .action(function (options) {
-        launcher(backfiller, [], options)
-      })
+  return {
+    name: 'backfill',
+    define: function () {
+      program
+        .command('backfill')
+        .description('2. run the backfiller')
+        .action(function (options) {
+          launcher(backfiller, [], options)
+        })
+    }
   }
 }
