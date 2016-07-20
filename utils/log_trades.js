e349138997cb98d5499232e934a9160623c88b73
Carlos Rodriguez <carlos@s8f.org>
2016-07-20 12:56:04 -0700

    fix

--- utils/log_trades.js
+++ utils/log_trades.js
@@ -18,6 +18,6 @@ module.exports = function container (get, set, clear) {
       return z(4, (trade.side === 'buy' ? 'SELL' : 'BUY') , ' ')[trade.side === 'buy' ? 'red' : 'green'] + ' ' + z(9, n(trade.size).format('0.000'), ' ')[trade.side === 'buy' ? 'red' : 'green'] + ' ' + config.asset.grey + ' at '.grey + z(9, n(trade.price).format('0.000'), ' ').yellow + ' ' + config.currency.grey
     }).join(', ')
     ticker = get_timestamp(min_time).grey + ' ' + ticker
-    get('logger').info(slug, 'processed ' + z(5, trades.length, ' ') + ' trades. '.grey + ticker)
+    get('logger').info(slug, ('processed ' + z(5, trades.length, ' ') + ' trades. ').grey + ticker)
   }
 }
