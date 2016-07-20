c086c99535bdd9918cb03bd038e18590e47021e8
Carlos Rodriguez <carlos@s8f.org>
2016-07-20 00:27:20 -0700

    wip

--- plugins/core/backfill/backfill.js
+++ plugins/core/backfill/backfill.js
@@ -9,53 +9,7 @@ module.exports = function container (get, set, clear) {
     var rs = get('run_state')
     var c = get('constants')
     var config = get('config')
-    rs.tick = tb(c.tick_size).toString()
-    var tasks = get('exchanges').map(function (exchange) {
-      return function task (cb) {
-        var products = get_products(exchange)
-        var tasks = products.map(function (product) {
-          return function task (done) {
-            try {
-              var backfiller = get('exchanges.' + exchange.name + '.backfiller')
-            }
-            catch (e) {
-              return done(null, [])
-            }
-            backfiller(product.id, options.limit, function (err, trades) {
-              if (err) {
-                err.exchange = exchange.name
-                return done(err)
-              }
-              var tasks = trades.map(function (trade) {
-                return function task (done) {
-                  trade.id = exchange.name + '-' + config.asset + '-' + config.currency + '-' + trade.id
-                  trade.asset = config.asset
-                  trade.currency = config.currency
-                  trade.exchange = exchange.name
-                  trade.processed = false
-                  get('motley:db.trades').save(trade, done)
-                }
-              })
-              parallel(tasks, function (err) {
-                if (err) return done(err)
-                log_trades(exchange.name, trades)
-                done()
-              })
-            })
-          }
-        })
-        parallel(tasks, cb)
-      }
-    })
-    parallel(tasks, function (err) {
-      if (err) {
-        get('logger').error('[' + err.exchange + ']', err.message, {public: false})
-      }
-      var timeout = setTimeout(function () {
-        backfill(options)
-      }, c.backfill_timeout)
-      set('timeouts[]', timeout)
-    })
-  }
-  return backfill
+    var tasks = get('backfillers')
+    
+
 }
--- plugins/core/db/db/trades.js
+++ plugins/core/db/db/trades.js
@@ -1,11 +1,13 @@
 module.exports = function container (get, set) {
   var get_timestamp = get('zenbot:utils.get_timestamp')
   var get_id = get('zenbot:utils.get_id')
+  var config = get('config')
   return get('db.createCollection')('trades', {
-    save: function (obj, opts, cb) {
-      if (!obj.id) obj.id = get_id()
-      obj.timestamp = get_timestamp(obj.time)
-      cb(null, obj)
+    save: function (trade, opts, cb) {
+      trade.id = trade.exchange + '-' + trade.id
+      trade.processed = false
+      trade.timestamp = get_timestamp(trade.time)
+      cb(null, trade)
     }
   })
 }
