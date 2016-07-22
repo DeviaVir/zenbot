f94450a444634d0cc9c0a09f4ecf1abc18394018
Dave Barnum <dave@thebarnums.com>
2016-07-21 23:16:46 -0400

    Add bitfinex exchange support.

--- plugins/bitfinex/_codemap.js
+++ plugins/bitfinex/_codemap.js
@@ -1,1 +1,7 @@
-_codemap.js
+module.exports = {
+  _ns: 'zenbot',
+  'exchanges.bitfinex': require('./exchange.json'),
+  'exchanges[]': '#exchanges.bitfinex',
+  'backfillers[]': require('./backfiller'),
+  'recorders[]': require('./recorder')
+}
