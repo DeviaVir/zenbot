ea825fcd3131ba9e29b61b9a71204e33f50c0b26
Carlos Rodriguez <carlos@s8f.org>
2016-07-30 21:29:55 -0700

    gdax backfiller sim chunk detection

--- config.js
+++ config.js
@@ -1,4 +1,15 @@
 var config = module.exports = {}
+config.mongo_url = "mongodb://localhost:27017/zenbrain" // change if your mongo server isn't local
+config.mongo_username = null // normally not needed
+config.mongo_password = null
+config.twitter_key = '' // create a twitter app, generate an access token, and add it here
+config.twitter_secret = ''
+config.twitter_access_token = ''
+config.twitter_access_token_secret = ''
+config.id_bytes = 8 // for random IDs
+config.save_state_interval = 10000 // save state
+config.parallel_limit = 8 // run this many concurrent tasks
+config.reduce_timeout = 1000
 config.asset = 'BTC'
 config.currency = 'USD'
 config.reducer_report_interval = 30000
@@ -6,9 +17,12 @@ config.enabled_plugins = [
   'bitfinex',
   'gdax'
 ]
-config.sim_chunk_size = '90d'
+config.sim_chunk_size = '7d'
+config.sim_chunks_required = 12
 config.max_slug_length = 22
 config.backfill_status_check = '30s'
 config.backfill_status_timeout = 35000
-config.reducer_limit = 100
-config.tick_sizes = ["10s"] // each tick size has its own map/reduce job
+config.reducer_limit = 5000
+config.tick_sizes = ['1m'] // each tick size has its own map/reduce job
+config.brain_speed = '1m' // at what speed should thinking occur
+config.brain_speed_ms = 60000
--- utils/backfill_status.js
+++ utils/backfill_status.js
@@ -6,23 +6,32 @@ module.exports = function container (get, set, clear) {
   var c = get('config')
   var current_period = null
   return function backfill_status (exchange, cb, isComplete) {
+    var rs = get('run_state')
+    rs[exchange.name] || (rs[exchange.name] = {sim_chunks: {}})
+    var sim_chunk_bucket = tb().resize(c.sim_chunk_size).subtract(c.sim_chunks_required + 1)
+    var sim_chunk = rs[exchange.name].sim_chunks[sim_chunk_bucket.toString()]
+    if (sim_chunk) {
+      get('logger').info(z(c.max_slug_length, exchange.name + ' backfiller', ' '), 'tick'.grey, sim_chunk_bucket.toString(), 'complete with'.grey, sim_chunk, 'trades'.grey)
+      return
+    }
     var this_period = tb().resize(c.backfill_status_check).toString()
-    if (current_period && this_period === current_period && !isComplete) {
+    if (this_period === current_period) {
       return cb()
     }
     //get('logger').info(z(c.max_slug_length, exchange.name + ' backfiller', ' '), 'checking status')
     current_period = this_period
-    var sim_chunk_bucket = tb().resize(c.sim_chunk_size).subtract(1)
+    var sim_chunk_end = tb().resize(c.sim_chunk_size)
     get('db').collection('thoughts').count({
       app_name: get('app_name'),
       key: 'trade',
+      'value.exchange': exchange.name,
       time: {
         $lt: sim_chunk_bucket.toMilliseconds()
       }
-    }, function (err, count) {
+    }, function (err, num_before) {
       if (err) return cb(err)
       //get('logger').info(z(c.max_slug_length, exchange.name + ' backfiller', ' '), 'checked status')
-      if (!count) {
+      if (!num_before) {
         get('logger').info(z(c.max_slug_length, exchange.name + ' backfiller', ' '), 'tick'.grey, sim_chunk_bucket.toString(), 'still backfilling'.grey)
         return cb()
       }
@@ -30,12 +39,13 @@ module.exports = function container (get, set, clear) {
       get('db').collection('thoughts').count({
         app_name: get('app_name'),
         key: 'trade',
+        'value.exchange': exchange.name,
         time: {
-          $gte: sim_chunk_bucket.add(1).toMilliseconds()
+          $gte: sim_chunk_end.toMilliseconds()
         }
-      }, function (err, count) {
+      }, function (err, num_after) {
         if (err) return cb(err)
-        if (!count) {
+        if (!num_after) {
           get('logger').info(z(c.max_slug_length, exchange.name + ' backfiller', ' '), 'tick'.grey, sim_chunk_bucket.toString(), 'not complete yet'.grey)
           return cb()
         }
@@ -43,35 +53,38 @@ module.exports = function container (get, set, clear) {
         get('db').collection('thoughts').count({
           app_name: get('app_name'),
           key: 'trade',
+          'value.exchange': exchange.name,
           time: {
-            $lt: sim_chunk_bucket.toMilliseconds(),
-            $gte: sim_chunk_bucket.subtract(1).toMilliseconds()
+            $lt: sim_chunk_end.toMilliseconds(),
+            $gte: sim_chunk_bucket.toMilliseconds()
           }
-        }, function (err, count) {
+        }, function (err, num_in_chunk) {
           if (err) return cb(err)
           //console.error('gte', sim_chunk_bucket.toString())
-          if (!count) {
+          if (!num_in_chunk) {
             get('logger').info(z(c.max_slug_length, exchange.name + ' backfiller', ' '), 'tick'.grey, sim_chunk_bucket.toString(), 'has no trades'.grey)
             return cb()
           }
           get('db').collection('thoughts').count({
             app_name: get('app_name'),
             key: 'trade',
+            'value.exchange': exchange.name,
             processed: false,
             time: {
-              $lt: sim_chunk_bucket.add(1).toMilliseconds(),
-              $gte: sim_chunk_bucket.subtract(1).toMilliseconds()
+              $lt: sim_chunk_end.toMilliseconds(),
+              $gte: sim_chunk_bucket.toMilliseconds()
             }
-          }, function (err, count) {
+          }, function (err, num_unprocssed) {
             if (err) return cb(err)
-            if (count) {
-              get('logger').info(z(c.max_slug_length, exchange.name + ' backfiller', ' '), 'tick'.grey, sim_chunk_bucket.toString(), 'still processing'.grey, count, 'trades'.grey)
-              return setTimeout(function () {
+            if (num_unprocssed) {
+              get('logger').info(z(c.max_slug_length, exchange.name + ' backfiller', ' '), 'tick'.grey, sim_chunk_bucket.toString(), 'still processing'.grey, num_unprocssed, 'trades'.grey)
+              setTimeout(function () {
                 backfill_status(exchange, cb, true)
               }, c.backfill_status_timeout)
             }
             else {
-              get('logger').info(z(c.max_slug_length, exchange.name + ' backfiller', ' '), 'tick'.grey, sim_chunk_bucket.toString(), 'complete with'.grey, count, 'trades'.grey)
+              rs[exchange.name].sim_chunks[sim_chunk_bucket.toString()] = num_in_chunk
+              get('logger').info(z(c.max_slug_length, exchange.name + ' backfiller', ' '), 'tick'.grey, sim_chunk_bucket.toString(), 'complete with'.grey, num_in_chunk, 'trades'.grey)
             }
           })
         })
