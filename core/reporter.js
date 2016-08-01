eda5abdbb3e05fac78114ba1949f3dfae8570a18
Carlos Rodriguez <carlos@s8f.org>
2016-07-31 22:46:55 -0700

    running

--- core/reporter.js
+++ core/reporter.js
@@ -1,12 +1,12 @@
 var colors = require('colors')
-  , parallel = require('run-parallel-limit')
+  , n = require('numbro')
 
 module.exports = function container (get, set, clear) {
   var c = get('config')
   return function reporter (tick, cb) {
     if (tick.size !== c.brain_speed) return cb()
     var rs = get('run_state')
-    get('logger').info('reporter', tick.id)
+    get('logger').info('reporter', tick.id.grey, String(tick.trades.trades).green, n(tick.trades.vol).format('0.000').white)
     cb()
   }
 }
--- core/thinker.js
+++ core/thinker.js
@@ -1,7 +1,7 @@
 module.exports = function container (get, set, clear) {
   return function thinker (tick, cb) {
     var rs = get('run_state')
-    get('logger').info('thinker', tick.id)
+    //get('logger').info('thinker', tick.id.grey, tick.trades.trades, tick.trades.vol)
     cb()
   }
 }
