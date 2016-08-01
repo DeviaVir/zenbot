ff39a6419b26a871b72b107d099943f3cdc13f79
Carlos Rodriguez <carlos@s8f.org>
2016-08-01 01:47:30 -0700

    zerofill

--- core/reporter.js
+++ core/reporter.js
@@ -1,5 +1,6 @@
 var colors = require('colors')
   , n = require('numbro')
+  , z = require('zero-fill')
 
 module.exports = function container (get, set, clear) {
   var c = get('config')
@@ -8,7 +9,7 @@ module.exports = function container (get, set, clear) {
     var rs = get('run_state')
     var tick_str = tick.id.split(':')[1]
     tick_str = tick_str.substring(0, tick_str.length - 2).grey + tick_str.substring(tick_str.length - 2).cyan
-    get('logger').info('reporter', tick_str, String(tick.trades.trades).grey, n(tick.trades.vol).format('0.000').white, n(tick.trades.avg_price).format('0.00').yellow, c.currency.grey)
+    get('logger').info(z(c.max_slug_length, 'reporter', ' '), tick_str, String(tick.trades.trades).grey, n(tick.trades.vol).format('0.000').white, n(tick.trades.avg_price).format('0.00').yellow, c.currency.grey)
     cb()
   }
 }
