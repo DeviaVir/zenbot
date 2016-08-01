bf1026443f7997dd84ca477908f3136243dd4912
Carlos Rodriguez <carlos@s8f.org>
2016-08-01 00:53:06 -0700

    ok

--- core/reporter.js
+++ core/reporter.js
@@ -6,7 +6,9 @@ module.exports = function container (get, set, clear) {
   return function reporter (tick, cb) {
     if (tick.size !== c.brain_speed) return cb()
     var rs = get('run_state')
-    get('logger').info('reporter', tick.id.grey, String(tick.trades.trades).green, n(tick.trades.vol).format('0.000').white)
+    var tick_str = tick.id.split(':')[1]
+    tick_str = tick_str.substring(0, tick_str.length - 2).grey + tick_str.substring(tick_str.length - 2).cyan
+    get('logger').info('reporter', tick_str, String(tick.trades.trades).grey, n(tick.trades.vol).format('0.000').white, n(tick.trades.avg_price).format('0.00').yellow, c.currency.grey)
     cb()
   }
 }
