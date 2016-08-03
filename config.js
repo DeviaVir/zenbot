0047450691345f588316f74a05cb07ffaf8902e1
Carlos Rodriguez <carlos@s8f.org>
2016-08-02 20:59:40 -0700

    graph test

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ config.asset = 'BTC'
 config.currency = 'USD'
 config.reducer_report_interval = 30000
 config.enabled_plugins = [
-  'bitfinex',
+  //'bitfinex',
   'gdax'
 ]
 config.sim_chunk_size = '7d'
