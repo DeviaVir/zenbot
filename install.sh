5fbd70a564a5c1053beecdb864802dea61f499dd
Carlos Rodriguez <carlos@s8f.org>
2016-08-04 02:41:21 -0700

    fix

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@ git clone https://github.com/carlos8f/zenbrain.git
 git clone https://github.com/carlos8f/zenbot.git
 cd zenbrain
 npm install
-npm link
+sudo npm link
 cd ../zenbot
 git checkout 3.x
 npm install
 npm link zenbrain
 rm -Rf /usr/local/bin/zenbot
-npm link
+sudo npm link
 zenbot launch map --backfill reduce run ticker_server
