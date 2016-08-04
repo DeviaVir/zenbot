e6682c054cd87d75d069fcbb66e62f700f871c21
Carlos Rodriguez <carlos@s8f.org>
2016-08-04 15:35:58 -0700

    report on other tick sizes

--- core/reporter.js
+++ core/reporter.js
@@ -5,13 +5,12 @@ var colors = require('colors')
 module.exports = function container (get, set, clear) {
   var c = get('config')
   return function reporter (tick, cb) {
-    if (tick.size !== c.brain_speed) return cb()
     var rs = get('run_state')
-    var tick_str = tick.id.split(':')[1]
+    var tick_str = z(12, tick.id.split(':')[1], ' ')
     tick_str = tick_str.substring(0, tick_str.length - 2).grey + tick_str.substring(tick_str.length - 2).cyan
     var slug = z(c.max_slug_length, 'reporter', ' ')
     //console.error("slug", slug)
-    get('logger').info(slug, tick_str, z(4, tick.trades.trades, ' '), n(tick.trades.vol).format('0.000').white, n(tick.trades.typical).format('0.00').yellow, c.currency.grey, rs.rsi[c.brain_speed].ansi)
+    get('logger').info(slug, tick_str, z(7, tick.trades.trades, ' '), n(tick.trades.vol).format('0.000').white, n(tick.trades.typical).format('0.00').yellow, c.currency.grey, 'RSI:'.grey + rs.rsi[c.brain_speed].ansi)
     cb()
   }
 }
