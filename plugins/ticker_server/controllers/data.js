0badeed810c989d4c43eb947590ca92b278ecaa8
Carlos Rodriguez <carlos@s8f.org>
2016-08-05 01:58:20 -0700

    exchange select

--- plugins/ticker_server/controllers/data.js
+++ plugins/ticker_server/controllers/data.js
@@ -6,12 +6,16 @@ module.exports = function container (get, set) {
   return get('controller')()
     .get('/data.csv', function (req, res, next) {
       res.setHeader('Content-Type', 'text/csv')
-      res.write('Time,Open,High,Low,Close,Volume,Close_str\n')
+      res.write('Time,Open,High,Low,Close,Volume,Close_str,Exchange\n')
+      var exchange = req.query.exchange || c.exchanges[0]
       get('db.ticks').select(
       {
         query: {
           app_name: get('zenbrain:app_name'),
-          size: req.query.period ? req.query.period : c.default_graph_period
+          size: req.query.period ? req.query.period : c.default_graph_period,
+          'exchanges.' + exchange: {
+            $exists: true
+          }
         },
         sort: {
           time: -1
@@ -23,12 +27,13 @@ module.exports = function container (get, set) {
           var date = moment(tick.time).format('D-MMM-YY')
           var line = [
             tick.time,
-            n(tick.trades.open).format('0.00'),
-            n(tick.trades.high).format('0.00'),
-            n(tick.trades.low).format('0.00'),
-            n(tick.trades.close).format('0.00'),
+            n(tick.trades[exchange].open).format('0.00'),
+            n(tick.trades[exchange].high).format('0.00'),
+            n(tick.trades[exchange].low).format('0.00'),
+            n(tick.trades[exchange].close).format('0.00'),
             tick.trades.vol,
-            c.currency_symbol + n(tick.trades.close).format(',0.00')
+            c.currency_symbol + n(tick.trades.close).format(',0.00'),
+            exchange.toUpperCase()
           ].join(',')
           res.write(line + '\n')
         })
