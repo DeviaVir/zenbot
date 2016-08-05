b7a8bec861dba850fc05e6bd083e3b86d079b75e
Carlos Rodriguez <carlos@s8f.org>
2016-08-04 22:10:22 -0700

    fix

--- plugins/ticker_server/action.js
+++ plugins/ticker_server/action.js
@@ -7,7 +7,7 @@ module.exports = function container (get, set, clear) {
     app.listen(function (err) {
       if (err) throw err
       var port = get('motley:site.server').address().port
-      get('logger').info(z(c.max_slug_length, 'ticker server', ' '), 'open'.grey, ('http://localhost:' + port + '/?period=' + c.default_graph_period + '&limit=' + c.default_graph_limit).yellow, 'to see a live graph.'.grey)
+      get('logger').info(z(c.max_slug_length, 'ticker server', ' '), 'open'.grey, ('http://localhost:' + port + '/').yellow, 'to see a live graph.'.grey)
     })
   }
 }
