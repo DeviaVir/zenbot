eb4106622fded67c56a1db63300ec6aa2064b1b8
Carlos Rodriguez <carlos@s8f.org>
2016-08-06 15:56:03 -0700

    stop backfill

--- plugins/gdax/backfiller.js
+++ plugins/gdax/backfiller.js
@@ -30,13 +30,16 @@ module.exports = function container (get, set, clear) {
         rs.resume_target = rs.backfiller_start
         rs.backfiller_start = null
       }
+      else {
+        rs.backfilled = 0
+      }
     }
     function retry () {
       setImmediate(mapper)
     }
     var uri = x.rest_url + '/products/' + product_id + '/trades?limit=' + x.backfill_limit + (rs.backfiller_id ? '&after=' + rs.backfiller_id : '')
     function withResult (result) {
-      var max_id
+      var max_id, min_time
       var trades = result.map(function (trade) {
         if (!rs.backfiller_start) {
           rs.backfiller_start = trade.trade_id
@@ -57,6 +60,8 @@ module.exports = function container (get, set, clear) {
           exchange: x.name
         }
         max_id = max_id ? Math.max(obj.trade_id, max_id) : max_id
+        min_time = min_time ? Math.min(obj.time, min_time) : min_time
+        rs.backfilled++
         map('trade', obj)
         return obj
       })
@@ -64,7 +69,12 @@ module.exports = function container (get, set, clear) {
         rs.backfiller_start = max_id
       }
       //log_trades(x.name, trades)
-      retry()
+      if (min_time < c.backfill_stop) {
+        get('logger').info(x.name, 'backfill complete with'.grey, rs.backfilled, 'trades.'.grey)
+      }
+      else {
+        retry()
+      }
     }
     //get('logger').info(z(c.max_slug_length, 'backfiller GET', ' '), uri.grey)
     request(uri, {headers: {'User-Agent': USER_AGENT}}, function (err, resp, result) {
