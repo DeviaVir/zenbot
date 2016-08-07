530e95ae8f42549862230a8ab83082d8377b8211
Carlos Rodriguez <carlos@s8f.org>
2016-08-06 22:35:50 -0700

    fix backfiller

--- plugins/gdax/backfiller.js
+++ plugins/gdax/backfiller.js
@@ -24,11 +24,12 @@ module.exports = function container (get, set, clear) {
     rs = rs.gdax
     if (first_run) {
       first_run = false
+      rs.backfiller_id = null
       if (rs.backfiller_id) {
-        rs.old_backfiller_id = rs.backfiller_id
-        rs.backfiller_id = null
-        rs.resume_target = rs.backfiller_start
-        rs.backfiller_start = null
+        //rs.old_backfiller_id = rs.backfiller_id
+        //rs.backfiller_id = null
+        //rs.resume_target = rs.backfiller_start
+        //rs.backfiller_start = null
       }
       else {
         rs.backfilled = 0
