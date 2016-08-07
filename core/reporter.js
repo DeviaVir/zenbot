6eda96c4c699652bafa17f4d452b3960cff5276a
Carlos Rodriguez <carlos@s8f.org>
2016-08-07 00:17:28 -0700

    fix spacing

--- core/reporter.js
+++ core/reporter.js
@@ -9,7 +9,7 @@ module.exports = function container (get, set, clear) {
   return function reporter (tick, cb) {
     var rs = get('run_state')
     //if (tick.time < start) return cb()
-    var tick_str = z(9, tick.id.split(':')[1], ' ')
+    var tick_str = z(10, tick.id.split(':')[1], ' ')
     tick_str = tick_str.substring(0, tick_str.length - 2).grey + tick_str.substring(tick_str.length - 2).cyan
     var slug = z(c.max_slug_length, 'reporter', ' ')
     var rsi = ''
