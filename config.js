8b1e29c40f92be1cf2a09f6d4d2c1cfb06be6ad0
Carlos Rodriguez <carlos@s8f.org>
2016-08-08 03:07:22 -0700

    bitfinex graph

--- config.js
+++ config.js
@@ -27,6 +27,14 @@ c.default_graph_period = "15m"
 c.default_graph_limit = 300
 c.graph_limits = [50, 100, 150, 200, 300, 500, 1000, 2000]
 c.graph_selectors = [
+  "bitfinex.BTC-USD",
+  "bitfinex.ETH-USD",
+  "bitfinex.ETH-BTC",
+  "bitfinex.ETC-BTC",
+  "bitfinex.LTC-USD",
+  "bitfinex.LTC-BTC",
+  "bitfinex.ETC-BTC",
+  "bitfinex.ETC-USD",
   "gdax.BTC-USD",
   "gdax.BTC-EUR",
   "gdax.ETH-USD",
