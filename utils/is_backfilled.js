28d1d3c99523731c6cffea3ad6d49e95d04d849a
Carlos Rodriguez <carlos@s8f.org>
2016-08-08 11:00:41 -0700

    fix

--- utils/is_backfilled.js
+++ utils/is_backfilled.js
@@ -3,6 +3,7 @@ var tb = require('timebucket')
 module.exports = function container (get, set, clear) {
   var c = get('config')
   return function is_backfilled (trades) {
+    if (!trades || !trades.length) return false
     var min_time = trades.reduce(function (prev, curr) {
       return prev ? Math.min(prev, curr.time) : curr.time
     })
