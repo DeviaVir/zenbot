3d32292976c8962a39b8a3a8640a7ca937943ea4
Carlos Rodriguez <carlos@s8f.org>
2016-08-12 21:38:38 -0700

    variables for asset/currency

--- core/indicators/tick_id/reporter_col.js
+++ core/indicators/tick_id/reporter_col.js
@@ -6,7 +6,7 @@ module.exports = function container (get, set, clear) {
   var z = get('utils.zero_fill')
   var get_tick_str = get('utils.get_tick_str')
   return function reporter_col (g, cb) {
-    g.cols.push(z(11, get_tick_str(g.tick.id)))
+    g.cols.push(z(12, get_tick_str(g.tick.id)))
     cb()
   }
 }
