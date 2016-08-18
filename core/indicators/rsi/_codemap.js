0d8ef8be3c072328060c6e3077ede2d798668c01
Carlos Rodriguez <carlos@s8f.org>
2016-08-18 10:44:00 -0700

    fix rsi backfill not triggered after backfill complete

--- core/indicators/rsi/_codemap.js
+++ core/indicators/rsi/_codemap.js
@@ -1,7 +1,6 @@
 module.exports = {
   _ns: 'zenbrain',
   'mappers.rsi_backfiller': require('./backfiller'),
-  'mappers[]': '#mappers.rsi_backfiller',
   'reporter_cols.rsi': require('./reporter_col'),
   'tick_reducers[10]': require('./tick_reducer')
 }
