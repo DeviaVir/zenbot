3c5e0646da2ebce05963314485556228c856e78a
Carlos Rodriguez <carlos@s8f.org>
2016-08-18 17:25:58 -0700

    show 1h rsi in reporter

--- core/indicators/rsi/reporter_col.js
+++ core/indicators/rsi/reporter_col.js
@@ -1,18 +1,26 @@
 var o = require('object-get')
   , colors = require('colors')
+  , tb = require('timebucket')
 
 module.exports = function container (get, set, clear) {
   var c = get('config')
   var z = get('utils.zero_fill')
   return function reporter_col (g, cb) {
-    var rsi = o(g.tick, 'data.trades.' + c.rsi_reporter_selector + '.rsi')
-    if (rsi) {
-      var line = ' RSI'.grey + z(3, rsi.ansi) + 'x'.grey + z(2, rsi.samples).grey
-      g.cols.push(line)
-    }
-    else {
-      g.cols.push(z(10, ' '))
-    }
-    cb()
+    var tick = g.tick, rs = g.rs
+    if (!rs.rsi_period || !rs.selector) return cb()
+    // get rsi
+    var rsi_tick_id = tb(tick.time).resize(rs.rsi_period).toString()
+    get('ticks').load(get('app_name') + ':' + rs.rsi_tick_id, function (err, rsi_tick) {
+      if (err) return cb(err)
+      var rsi = o(rsi_tick || {}, rs.selector + '.rsi')
+      if (rsi) {
+        var line = (rs.rsi_period + ' RSI').grey + z(3, rsi.ansi) + 'x'.grey + z(2, rsi.samples).grey
+        g.cols.push(line)
+      }
+      else {
+        g.cols.push(z(10, ' '))
+      }
+      cb()
+    })
   }
 }
