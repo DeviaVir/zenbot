4149b6b82e26b6a82422dbe9d674553dccd7341a
Carlos Rodriguez <carlos@s8f.org>
2016-08-21 16:21:39 -0700

    fix

--- plugins/server/controllers/data.js
+++ plugins/server/controllers/data.js
@@ -3,7 +3,6 @@ var moment = require('moment')
   , o = require('object-get')
 
 module.exports = function container (get, set) {
-  var c = get('zenbrain:config')
   var format_currency = get('zenbrain:utils.format_currency')
   return get('controller')()
     .get('/sim_trades.csv', function (req, res, next) {
@@ -60,6 +59,7 @@ module.exports = function container (get, set) {
       })
     })
     .get('/data.csv', function (req, res, next) {
+      var c = get('zenbrain:config')
       res.setHeader('Content-Type', 'text/csv')
       res.write('Time,Open,High,Low,Close,Volume,Caption\n')
       var selector = req.query.selector ? req.query.selector : c.graph_selectors[0]
