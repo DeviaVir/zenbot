6787d6e5b6fc35eb2a1823c4780a9d29d43d6714
Carlos Rodriguez <carlos@s8f.org>
2016-08-23 12:20:49 -0700

    fix run script not starting server

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# all-in-one single-pair run script using BTC/USD pair
+# all-in-one single-pair run script using BTC/USD pair.
 # change to a different config to run a different pair.
 
-./zenbot launch map --backfill reduce run --verbose server --config config_btc_usd.js
+./zenbot launch map --backfill reduce run server --verbose --config config_btc_usd.js
