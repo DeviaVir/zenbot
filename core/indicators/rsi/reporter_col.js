aa4ccd9de64bef2e4586cec0436befa10cfeceb3
Carlos Rodriguez <carlos@s8f.org>
2016-08-23 02:11:29 -0700

    make use of rs.rsi for indicators, spacing for eta

--- core/indicators/rsi/reporter_col.js
+++ core/indicators/rsi/reporter_col.js
@@ -7,20 +7,13 @@ module.exports = function container (get, set, clear) {
   return function reporter_col (g, cb) {
     var c = get('config')
     var tick = g.tick, rs = g.rs
-    // get rsi
-    var rsi_tick_id = tb(tick.time).resize(c.rsi_reporter_size).toString()
-    get('ticks').load(get('app_name') + ':' + rsi_tick_id, function (err, rsi_tick) {
-      if (err) return cb(err)
-      var rsi = o(rsi_tick || {}, rs.selector + '.rsi')
-      if (rsi) {
-        var line = 'RSI:'.grey + z(3, rsi.ansi)
-        g.cols.push(line)
-        rs.rsi = rsi
-      }
-      else {
-        g.cols.push('RSI:n/a'.grey)
-      }
-      cb()
-    })
+    if (rs.rsi) {
+      var line = 'RSI:'.grey + z(3, rs.rsi.ansi)
+      g.cols.push(line)
+    }
+    else {
+      g.cols.push('RSI:n/a'.grey)
+    }
+    cb()
   }
 }
