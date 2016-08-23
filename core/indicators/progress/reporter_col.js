502b941f74d19d40e4b261375e61dd7b4305ce9d
Carlos Rodriguez <carlos@s8f.org>
2016-08-22 23:40:34 -0700

    fix progress reported when no trade made

--- core/indicators/progress/reporter_col.js
+++ core/indicators/progress/reporter_col.js
@@ -5,7 +5,7 @@ module.exports = function container (get, set, clear) {
   var z = get('utils.zero_fill')
   return function reporter_col (g, cb) {
     var c = get('config')
-    var line = g.rs.trend ? ('trend:' + g.rs.trend).grey + (g.rs.progress < 1 ? ', HOLD progress '.grey + z(4, n(g.rs.progress).format('0%')).yellow : '') : '(no trend)'.grey
+    var line = g.rs.trend ? ('trend:' + g.rs.trend).grey + (g.rs.progress && g.rs.progress < 1 ? ', HOLD progress '.grey + z(4, n(g.rs.progress).format('0%')).yellow : '') : '(no trend)'.grey
     g.cols.push(line)
     cb()
   }
