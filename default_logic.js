c7d96bd915c4d24f5f342ecea38b02a6acc5c9c1
Carlos Rodriguez <carlos@s8f.org>
2016-08-29 22:02:44 -0700

    roi = 1.531, 43 trades

--- default_logic.js
+++ default_logic.js
@@ -73,7 +73,7 @@ module.exports = function container (get, set, clear) {
       if (!rs.product) return cb(new Error('no product for ' + c.default_selector))
       rs.min_trade = n(rs.product.min_size).multiply(1).value()
       rs.sim_start_balance = 10000
-      rs.min_double_wait = 86400000 * 2 // wait in ms after action before doing same action
+      rs.min_double_wait = 86400000 * 1 // wait in ms after action before doing same action
       rs.min_reversal_wait = 86400000 * 0.75 // wait in ms after action before doing opposite action
       rs.min_performance = -0.015 // abort trades with lower performance score
       if (first_run) {
