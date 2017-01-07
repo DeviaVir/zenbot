8bd74b48e2d26b662753fc658b1715771637a352
Tobias Abenius <tobbe@tobbe.nu>
2017-01-07 20:03:23 +0100

    Easier to get started with Kraken trading

--- config_btc_usd.js
+++ config_btc_usd.js
@@ -1,13 +1,21 @@
 var c = module.exports = require('./config')
 
 c.assets = [
-  "BTC"
+  "BTC" // for GDAX
+  //"XXBT" // for kraken
 ]
 c.currencies = [
+  // for GDAX
   "USD",
   "USDT",
-  "BTC"
+  "BTC",
+  // for kraken
+  /*
+  "ZUSD",
+  "XXBT"
+  */
 ]
 
-// default selector for indicators, etc
+// default selector for indicators, trading, etc
 c.default_selector = "gdax.BTC-USD"
+//c.default_selector = "kraken.XXBT-ZUSD"
--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 # all-in-one single-pair run script using BTC/USD pair.
 # change to a different config to run a different pair.
 
-./zenbot launch map reduce run server --config config_btc_usd.js
+./zenbot launch map --backfill reduce run server --config config_btc_usd.js
