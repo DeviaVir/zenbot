0853961bdebf435da7dfa978d67a03c2fd458461
nedievas <nedievas@outlook.com>
2017-02-18 18:28:38 +0200

    Network IP

--- plugins/server/action.js
+++ plugins/server/action.js
@@ -1,5 +1,4 @@
 var z = require('zero-fill')
-
 module.exports = function container (get, set, clear) {
   var get_id = get('zenbrain:utils.get_id')
   return function action () {
@@ -7,11 +6,21 @@ module.exports = function container (get, set, clear) {
     var app = get('app')
     var secret = get_id()
     set('@zenbrain:secret', secret)
+    /*Get Network IP for Sim results*/ 
+      var os = require('os');
+      var ifaces = os.networkInterfaces();
+      var values = Object.keys(ifaces).map(function(name) {
+        return ifaces[name];
+      });
+      values = [].concat.apply([], values).filter(function(val){ 
+        return val.family === 'IPv4' && val.internal === false; 
+      });
+      var ip = values.length ? values[0].address : '127.0.0.1';
     app.listen(function (err) {
       if (err) throw err
       get('logger').info('server', USER_AGENT, 'booted!'.cyan)
       var port = get('motley:site.server').address().port
-      get('logger').info('server', 'open'.grey, ('http://localhost:' + port + '/?secret=' + secret).yellow, 'to see a live graph.'.grey)
+      get('logger').info('server', 'open'.grey, ('http://' + ip + ':' + port + '/?secret=' + secret).yellow, 'to see a live graph.'.grey)
     })
   }
 }
