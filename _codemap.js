0c5836553a5ef3b093057f1170b33abe1f588a29
Morpheus C. Door <carlos8f@me.com>
2017-04-20 16:17:05 -0700

    lib func for list-selectors

--- _codemap.js
+++ _codemap.js
@@ -2,7 +2,8 @@ module.exports = {
   _ns: 'zenbot',
   _maps: [
     require('./commands/_codemap'),
-    require('./db/_codemap')
+    require('./db/_codemap'),
+    require('./lib/_codemap')
   ],
   'conf': null,
   'exchanges[]': []
