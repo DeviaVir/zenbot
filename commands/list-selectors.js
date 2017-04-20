0eabaf9892f13fd9982f8d4592b220c47889c6e1
Morpheus C. Door <carlos8f@me.com>
2017-04-20 16:17:05 -0700

    lib func for list-selectors

--- commands/list-selectors.js
+++ commands/list-selectors.js
@@ -7,20 +7,11 @@ module.exports = function container (get, set, clear) {
       .command('list-selectors')
       .description('list available selectors to watch')
       .action(function (cmd) {
-        var exchanges = get('exchanges.list')
-        var tasks = {}
-        exchanges.forEach(function (exchange) {
-          tasks[exchange.name] = exchange.getProducts
-        })
-        parallel(tasks, function (err, results) {
+        get('lib.list-selectors')(function (err, selectors) {
           if (err) throw err
-          Object.keys(results).forEach(function (exchange_name) {
-            console.log()
-            console.log(exchange_name)
-            console.log()
-            results[exchange_name].forEach(function (product_id) {
-              console.log('  ' + exchange_name + '.' + product_id)
-            })
+          console.log()
+          selectors.forEach(function (selector) {
+            console.log('  ' + selector)
           })
           console.log()
           process.exit(0)
