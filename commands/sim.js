403b4aa80c6606f1a4e9766561b0671e1aa16431
Carlos Rodriguez <carlos@s8f.org>
2017-04-27 04:37:26 -0700

    fudge rule to prevent selling on big drops, 10.64% over buy/hold

--- commands/sim.js
+++ commands/sim.js
@@ -32,6 +32,7 @@ module.exports = function container (get, set, clear) {
           last_period_id: null,
           period_buffer: {},
           trend: null,
+          signal: null,
           start_position: 50,
           buy_hold_start: null
         }
@@ -106,13 +107,43 @@ module.exports = function container (get, set, clear) {
                     var multiplier = 2 / (strategy.options[k] + 1)
                     s.period_buffer[k] = (s.period_buffer.close - prev_ema) * multiplier + prev_ema
                   })
+                  var avg_gain = s.lookback[0].avg_gain
+                  var avg_loss = s.lookback[0].avg_loss
+                  if (typeof avg_gain === 'undefined') {
+                    var gain_sum = 0
+                    var loss_sum = 0
+                    var last_close
+                    s.lookback.slice(0, strategy.options.rsi_periods).forEach(function (period) {
+                      if (last_close) {
+                        if (period.close > last_close) {
+                          gain_sum += period.close - last_close
+                        }
+                        else {
+                          loss_sum += last_close - period.close
+                        }
+                      }
+                      last_close = period.close
+                    })
+                    s.period_buffer.avg_gain = gain_sum / strategy.options.rsi_periods
+                    s.period_buffer.avg_loss = loss_sum / strategy.options.rsi_periods
+                  }
+                  else {
+                    var current_gain = s.period_buffer.close - s.lookback[0].close
+                    s.period_buffer.avg_gain = ((avg_gain * (strategy.options.rsi_periods - 1)) + (current_gain > 0 ? current_gain : 0)) / strategy.options.rsi_periods
+                    var current_loss = s.lookback[0].close - s.period_buffer.close
+                    s.period_buffer.avg_loss = ((avg_loss * (strategy.options.rsi_periods - 1)) + (current_loss > 0 ? current_loss : 0)) / strategy.options.rsi_periods
+                  }
+                  var rs = s.period_buffer.avg_gain / s.period_buffer.avg_loss
+                  s.period_buffer.rsi = Math.round(100 - (100 / (1 + rs)))
                   if (s.period_buffer.trend_ema && s.lookback[0].trend_ema) {
                     if (s.period_buffer.trend_ema / s.lookback[0].trend_ema >= 1) {
                       s.trend = 'up'
+                      s.signal = 'buy'
                       s.trend_rate = (s.period_buffer.trend_ema - s.lookback[0].trend_ema) / s.lookback[0].trend_ema * 100
                     }
                     else {
                       s.trend = 'down'
+                      s.signal = 'sell'
                       s.trend_rate = (s.lookback[0].trend_ema - s.period_buffer.trend_ema) / s.period_buffer.trend_ema * -100
                     }
                     if (s.trend_rate >= 0 && s.trend_rate < 0.01) {
@@ -121,21 +152,40 @@ module.exports = function container (get, set, clear) {
                     else if (s.trend_rate < 0 && s.trend_rate > -0.01) {
                       s.trend = null
                     }
+                    if (s.signal === 'buy' && s.trend_rate >= 0.05) {
+                      //s.signal = null
+                    }
+                    else if (s.signal === 'sell' && s.trend_rate <= -0.05) {
+                      s.signal = null
+                    }
+                    /*
+                    if (s.period_buffer.rsi >= 70) {
+                      s.signal = 'sell'
+                    }
+                    else if (s.period_buffer.rsi <= 30) {
+                      s.signal = 'buy'
+                    }
+                    else {
+                      s.signal = null
+                    }
+                    */
                     var action
-                    if (s.trend === 'up') {
+                    if (s.signal === 'buy') {
                       var size = s.balance[currency] / trade.price
-                      if (size >= 0.01 && s.balance[currency] - (trade.price * size) >= 0)  {
+                      if (size >= 0.01 && s.balance[currency] - (trade.price * size) >= 0 && trade.price > 100)  {
                         s.balance[asset] += size
                         s.balance[currency] -= trade.price * size
                         action = 'bought'
+                        s.signal = null
                       }
                     }
-                    else if (s.trend === 'down') {
+                    else if (s.signal === 'sell') {
                       var size = s.balance[asset]
                       if (size >= 0.01 && s.balance[asset] - size >= 0 && trade.price > 100)  {
                         s.balance[asset] -= size
                         s.balance[currency] += trade.price * size
                         action = 'sold'
+                        s.signal = null
                       }
                     }
                     process.stdout.write(moment(s.period_buffer.time).format('YYYY-MM-DD HH').grey)
@@ -144,12 +194,16 @@ module.exports = function container (get, set, clear) {
                     process.stdout.write(z(8, n(diff).format('0.00'), ' ')[diff >= 0 ? 'green' : 'red'])
                     process.stdout.write(z(6, s.trend || 'null', ' ')[s.trend ? s.trend === 'up' ? 'green' : 'red' : 'grey'])
                     process.stdout.write(z(9, n(s.trend_rate).format('0.0000'), ' ')[s.trend ? s.trend === 'up' ? 'green' : 'red' : 'grey'])
+                    var rsi_color = 'grey'
+                    if (s.period_buffer.rsi >= 70) rsi_color = 'green'
+                    else if (s.period_buffer.rsi <= 30) rsi_color = 'red'
+                    process.stdout.write(z(3, s.period_buffer.rsi, ' ')[rsi_color])
                     process.stdout.write(z(9, action || 'null', ' ')[action ? action === 'bought' ? 'green' : 'red' : 'grey'])
                     process.stdout.write(z(9, n(s.balance[asset]).format('0.0000'), ' ').white)
                     process.stdout.write(z(10, n(s.balance[currency]).format('$0.00'), ' ').yellow)
                     var consolidated = s.balance[currency] + (trade.price * s.balance[asset])
                     var profit = (consolidated - strategy.options.start_capital) / strategy.options.start_capital
-                    process.stdout.write(z(7, n(profit).format('0.00%'), ' ')[profit >= 0 ? 'green' : 'red'])
+                    process.stdout.write(z(8, n(profit).format('0.00%'), ' ')[profit >= 0 ? 'green' : 'red'])
                     process.stdout.write('\n')
                   }
                 }
