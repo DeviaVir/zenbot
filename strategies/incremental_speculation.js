58a42e65d8da390143cc1ea429a0804a1d64a95f
Carlos Rodriguez <carlos@s8f.org>
2017-04-29 01:35:58 -0700

    sim model now uses limit orders, 19.34% over buy/hold

--- strategies/incremental_speculation.js
+++ strategies/incremental_speculation.js
@@ -5,6 +5,10 @@ module.exports = {
     trend_ema: 36,
     price_ema: 6,
     start_capital: 1000,
-    rsi_periods: 14
+    rsi_periods: 14,
+    markup_pct: 0.01,
+    markdown_pct: 0.01,
+    bid_adjust_time: 300000,
+    max_sell_loss_pct: -10
   }
 }
