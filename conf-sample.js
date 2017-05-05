d440102bd52abd688d8f82acde0e5c0c03dc2fb9
Carlos Rodriguez <carlos@s8f.org>
2017-05-05 00:13:26 -0700

    implement stops, 13.57% over buy/hold

--- conf-sample.js
+++ conf-sample.js
@@ -4,6 +4,14 @@ var c = module.exports = {}
 
 // name of default trade strategy. list available with `zenbot list-strategies` and override with (--strategy)
 c.strategy = 'trend_ema_rate'
+// sell if price drops below this % of bought price
+c.sell_stop_pct = 4
+// buy if price surges above this % of sold price
+c.buy_stop_pct = 4
+// enable trailing sell stop when reaching this % profit
+c.profit_stop_enable_pct = 3
+// maintain a trailing stop this % below the high-water mark of profit
+c.profit_stop_pct = 3
 // avoid selling at a loss below this pct (override with --max_sell_loss_pct)
 c.max_sell_loss_pct = -10
 
@@ -30,6 +38,8 @@ c.error_backoff = 30000
 // amount of currency to start simulations with (normally overriden with --start_capital)
 c.start_capital = 1000
 // % to markup price for sell orders (normally overriden with --markup_pct)
-c.markup_pct = 0.01
+c.markup_pct = 0
 // ms to adjust non-filled order after
 c.order_adjust_time = 300000
+// for sim, reverse time at the end of the graph, normalizing buy/hold to 0
+c.symmetrical = false
