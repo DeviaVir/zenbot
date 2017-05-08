ff51ec976a4d36d60e9fb1094d776733153d8a10
Carlos Rodriguez <carlos@s8f.org>
2017-05-07 19:16:53 -0700

    hide preroll reports

--- commands/sim.js
+++ commands/sim.js
@@ -47,7 +47,7 @@ module.exports = function container (get, set, clear) {
         var engine = get('lib.engine')(s)
         if (!so.min_periods) so.min_periods = 1
         var cursor, reversing, reverse_point
-        var query_start = so.start ? d.resize(so.period).subtract(so.min_periods).toMilliseconds() : null
+        var query_start = so.start ? d.resize(so.period).subtract(so.min_periods + 2).toMilliseconds() : null
 
         function exitSim () {
           console.log(so)
