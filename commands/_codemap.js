1623454fb2e7ebfe405d0393b29165de0ba54f4a
Carlos Rodriguez <carlos@s8f.org>
2017-05-08 21:56:29 -0700

    paper trading

--- commands/_codemap.js
+++ commands/_codemap.js
@@ -5,8 +5,10 @@ module.exports = {
   'list-selectors': require('./list-selectors'),
   'backfill': require('./backfill'),
   'sim': require('./sim'),
+  'trade': require('./trade'),
 
   'list[20]': '#commands.list-selectors',
   'list[50]': '#commands.backfill',
-  'list[60]': '#commands.sim'
+  'list[60]': '#commands.sim',
+  'list[70]': '#commands.trade'
 }
