9a2417f8b8dd676032ed68806d94ddc9faf43b21
Carlos Rodriguez <carlos@s8f.org>
2017-05-18 19:28:33 -0700

    tentative poloniex implementation

--- extensions/poloniex/exchange.js
+++ extensions/poloniex/exchange.js
@@ -1,6 +1,7 @@
 var Poloniex = require('poloniex.js')
   , path = require('path')
   , moment = require('moment')
+  , n = require('numbro')
 
 module.exports = function container (get, set, clear) {
   var c = get('conf')
@@ -34,18 +35,39 @@ module.exports = function container (get, set, clear) {
     return authed_client
   }
 
-  function statusErr (resp, body) {
-    if (resp.statusCode !== 200) {
-      var err = new Error('non-200 status: ' + resp.statusCode)
-      err.code = 'HTTP_STATUS'
-      err.body = body
-      return err
-    }
-  }
-
   function joinProduct (product_id) {
     return product_id.split('-')[1] + '_' + product_id.split('-')[0]
   }
+
+  function trade (type, opts, cb) {
+    var client = authedClient()
+    var params = {
+      currencyPair: joinProduct(opts.product_id),
+      rate: opts.price,
+      amount: opts.size,
+      postOnly: opts.post_only === false ? '0' : '1'
+    }
+    client._private(type, params, function (err, result) {
+      if (!err && result.error) {
+        err = new Error('unable to ' + type)
+        err.body = result
+      }
+      if (err) return cb(err)
+      var order = {
+        id: result.orderNumber,
+        status: 'open',
+        price: opts.price,
+        size: opts.size,
+        post_only: !!opts.post_only,
+        created_at: new Date().getTime(),
+        filled_size: '0'
+      }
+      orders['~' + result.orderNumber] = order
+      cb(null, order)
+    })
+  }
+
+  var orders = {}
 
   return {
     name: 'poloniex',
@@ -105,104 +127,69 @@ module.exports = function container (get, set, clear) {
     },
 
     getQuote: function (opts, cb) {
-      var args = _.toArray(arguments);
-      this.poloniex.getTicker(function(err, data) {
-        if(err)
-          return this.retry(this.getTicker, args);
-
-        var tick = data[this.pair];
-
-        callback(null, {
-          bid: parseFloat(tick.highestBid),
-          ask: parseFloat(tick.lowestAsk),
-        });
-
-      }.bind(this));
-
-
-      var client = publicClient(opts.product_id)
-      client.getProductTicker(function (err, resp, body) {
-        if (!err) err = statusErr(resp, body)
+      var client = publicClient()
+      var product_id = joinProduct(opts.product_id)
+      client.getTicker(function (err, body) {
         if (err) return cb(err)
-        cb(null, {bid: body.bid, ask: body.ask})
+        var quote = body[product_id]
+        if (!quote) return cb(new Error('no quote for ' + product_id))
+        if (quote.isFrozen == '1') return cb(new Error('product ' + product_id + ' is frozen'))
+        cb(null, {
+          bid: tick.highestBid,
+          ask: tick.lowestAsk,
+        })
       })
     },
 
     cancelOrder: function (opts, cb) {
-      var cancel = function(err, result) {
-        if(err || !result.success) {
-            log.error('unable to cancel order', order, '(', err, result, ')');
-          }
-        }.bind(this);
-
-      this.poloniex.cancelOrder(this.currency, this.asset, order, cancel);
-
       var client = authedClient()
-      client.cancelOrder(opts.order_id, function (err, resp, body) {
-        if (!err) err = statusErr(resp, body)
-        if (err) return cb(err)
-        cb()
+      client._private('cancelOrder', {orderNumber: opts.order_id}, function (err, result) {
+        if (!err && !result.success) {
+          err = new Error('unable to cancel order')
+          err.body = result
+        }
+        cb(err)
       })
     },
 
     buy: function (opts, cb) {
-      var set = function(err, result) {
-        if(err || result.error)
-          return log.error('unable to buy:', err, result);
-
-        callback(null, result.orderNumber);
-      }.bind(this);
-
-      this.poloniex.buy(this.currency, this.asset, price, amount, set);
-
-
-      var client = authedClient()
-      if (typeof opts.post_only === 'undefined') {
-        opts.post_only = true
-      }
-      client.buy(opts, function (err, resp, body) {
-        if (!err) err = statusErr(resp, body)
-        if (err) return cb(err)
-        cb(null, body)
-      })
+      trade('buy', opts, cb)
     },
 
     sell: function (opts, cb) {
-      var set = function(err, result) {
-        if(err || result.error)
-          return log.error('unable to sell:', err, result);
-
-        callback(null, result.orderNumber);
-      }.bind(this);
-
-      this.poloniex.sell(this.currency, this.asset, price, amount, set);
-
-
-      var client = authedClient()
-      if (typeof opts.post_only === 'undefined') {
-        opts.post_only = true
-      }
-      client.sell(opts, function (err, resp, body) {
-        if (!err) err = statusErr(resp, body)
-        if (err) return cb(err)
-        cb(null, body)
-      })
+      trade('sell', opts, cb)
     },
 
     getOrder: function (opts, cb) {
-      var check = function(err, result) {
-        var stillThere = _.find(result, function(o) { return o.orderNumber === order });
-        callback(err, !stillThere);
-      }.bind(this);
-
-      this.poloniex.myOpenOrders(this.currency, this.asset, check);
-
-
+      var order = orders['~' + opts.order_id]
+      if (!order) return cb(new Error('order not found in cache'))
       var client = authedClient()
-      client.getOrder(opts.order_id, function (err, resp, body) {
-        if (!err) err = statusErr(resp, body)
+      var params = {
+        currencyPair: joinProduct(opts.product_id)
+      }
+      client._private('returnOpenOrders', params, function (err, body) {
         if (err) return cb(err)
-        cb(null, body)
+        var active = false
+        body.forEach(function (api_order) {
+          if (api_order.orderNumber == opts.order_id) active = true
+        })
+        if (!active) {
+          order.status = 'done'
+          order.done_at = new Date().getTime()
+          return cb(null, order)
+        }
+        client.returnOrderTrades(opts.order_id, function (err, body) {
+          if (err) return cb(null, order)
+          order.filled_size = '0'
+          body.forEach(function (trade) {
+            order.filled_size = n(order.filled_size).add(trade.amount).format('0.00000000')
+          })
+          if (n(order.filled_size).value() == n(order.size).value()) {
+            order.status = 'done'
+            order.done_at = new Date().getTime()
+          }
+          cb(null, order)
+        })
       })
     },
 
