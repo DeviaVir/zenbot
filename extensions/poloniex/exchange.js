7c9950630532eb70056d8d6a9b04c743f58f1fd9
Carlos Rodriguez <carlos@s8f.org>
2017-05-20 18:47:01 -0700

    fix gaps in poloniex backfill

--- extensions/poloniex/exchange.js
+++ extensions/poloniex/exchange.js
@@ -41,7 +41,7 @@ module.exports = function container (get, set, clear) {
 
   var exchange = {
     name: 'poloniex',
-    historyScan: 'forward',
+    historyScan: 'backward',
     makerFee: 0.15,
 
     getProducts: function () {
@@ -63,6 +63,10 @@ module.exports = function container (get, set, clear) {
       if (args.start && !args.end) {
         // add 24 hours
         args.end = args.start + 86400
+      }
+      else if (args.end && !args.start) {
+        // subtract 24 hours
+        args.start = args.end - 86400
       }
 
       client._public('returnTradeHistory', args, function (err, body) {
