5cac19fba98b6a7898e4573bd4625e011977f779
Fjuxx <nickvanalst17@gmail.com>
2017-06-06 12:18:45 +0200

    Adjusted glob
    Adjusted the glob pattern to search recursive and already search for the _codemap.js file.
    
    This made the following function a lot simpler as glob does al the work for us.
    
    With this change the structure inside the \extensions\ folder does not matter anymore.

--- boot.js
+++ boot.js
@@ -18,14 +18,12 @@ module.exports = function (cb) {
   zenbot.set('@zenbot:conf', c)
 
   function withMongo () {
-    glob('extensions/*', {cwd: __dirname, absolute: true}, function (err, results) {
+    //searches all directorys in {workingdir}/extensions/ for files called '_codemap.js'
+    glob('extensions/**/_codemap.js', {cwd: __dirname, absolute: true}, function (err, results) {
       if (err) return cb(err)
       results.forEach(function (result) {
-        if (path.basename(result) === 'README.md') {
-          return
-        }
-        var ext = require(path.join(result, '_codemap'))
-        zenbot.use(ext)
+        var ext = require(result) //load the _codemap for the extension
+        zenbot.use(ext)           //load the extension into zenbot
       })
       cb(null, zenbot)
     })
