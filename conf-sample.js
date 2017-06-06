7cd8ad05bfc02fe910db7c52f1e4299de7b931f4
Chase <chase@sillevis.net>
2017-06-06 12:53:45 +0200

    Aesthetics for replicaSet

--- conf-sample.js
+++ conf-sample.js
@@ -8,7 +8,7 @@ c.mongo.db = 'zenbot4'
 c.mongo.username = null
 c.mongo.password = null
 // when using mongodb replication, i.e. when running a mongodb cluster, you can define your replication set here; when you are not using replication (most of the users), just set it to '' (default).
-c.mongo.replicaSet = ''
+c.mongo.replicaSet = null
 
 // default selector. only used if omitting [selector] argument from a command.
 c.selector = 'gdax.BTC-USD'
