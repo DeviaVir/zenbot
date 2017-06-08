00ce2aaaff6b54ec9d158ebb7577e261475ab72e
Carlos Rodriguez <carlos@s8f.org>
2017-06-08 11:48:13 -0700

    fix sar description

--- extensions/strategies/sar/strategy.js
+++ extensions/strategies/sar/strategy.js
@@ -4,7 +4,7 @@ var z = require('zero-fill')
 module.exports = function container (get, set, clear) {
   return {
     name: 'sar',
-    description: 'Buy when (EMA - last(EMA) > 0) and sell when (EMA - last(EMA) < 0). Optional buy on low RSI.',
+    description: 'Parabolic SAR',
 
     getOptions: function () {
       this.option('period', 'period length', String, '5m')
