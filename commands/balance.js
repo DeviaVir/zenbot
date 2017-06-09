eb14db9306d2ac8b72dd02b157d1c400068c6860
eigilb <eigil@bjorgum.no>
2017-06-09 11:34:54 +0200

    New command "zenbot balance <selector>" (#243)
    * Add files via upload
    
    * Delete exchanges.md
    
    * Create exchanges.md
    
    * Delete exchanges.md
    
    * Create developers.md
    
    * Update developers.md
    
    * Update developers.md
    
    * New command "zenbot balance <selector>"
    
    Introduces a new comand:
    "zenbot balance <selector>"
    Sample output: 
    BTC-USD Asset: 9.87654321. Currency: 1.23
    
    * New command "zenbot balance <selector>"

---
+++ commands/balance.js
@@ -0,0 +1,41 @@
+var minimist = require('minimist')
+  , n = require('numbro')
+  , colors = require('colors')
+
+module.exports = function container (get, set, clear) {
+  var c = get('conf')
+  return function (program) {
+    program
+      .command('balance [selector]')
+      .allowUnknownOption()
+      .description('get asset and currency balance from the exchange')
+      //.option('--all', 'output all balances')
+      .option('--debug', 'output detailed debug info')
+      .action(function (selector, cmd) {
+        var s = {options: minimist(process.argv)}
+        s.selector = get('lib.normalize-selector')(selector || c.selector)
+        var exch = s.selector.split('.')[0]
+        s.exchange = get('exchanges.' + exch)
+        s.product_id = s.selector.split('.')[1]
+        s.asset = s.product_id.split('-')[0]
+        s.currency = s.product_id.split('-')[1]
+        var so = s.options
+        delete so._
+        Object.keys(c).forEach(function (k) {
+          if (typeof cmd[k] !== 'undefined') {
+            so[k] = cmd[k]
+          }
+        })
+        so.debug = cmd.debug
+        function balance () {
+          s.exchange.getBalance(s, function (err, balance) {
+            if (err) return cb(err)
+            var bal = s.product_id + ' Asset: ' + balance.asset + ' Currency: ' + balance.currency
+            console.log(bal)
+            process.exit()
+          })
+        }
+        balance()
+      })
+  }
+}
