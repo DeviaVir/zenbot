a92e5db4d6997194b91e4e90df80b63429d47d45
Chase Sillevis <chase@sillevis.net>
2017-06-10 18:38:45 +0200

    Calculate balance: translate assets/currency to something else

--- commands/balance.js
+++ commands/balance.js
@@ -10,6 +10,7 @@ module.exports = function container (get, set, clear) {
       .allowUnknownOption()
       .description('get asset and currency balance from the exchange')
       //.option('--all', 'output all balances')
+      .option('-c, --calculate_currency <calculate_currency>', 'show the full balance in another currency')
       .option('--debug', 'output detailed debug info')
       .action(function (selector, cmd) {
         var s = {options: minimist(process.argv)}
@@ -29,15 +30,33 @@ module.exports = function container (get, set, clear) {
         so.debug = cmd.debug
         function balance () {
           s.exchange.getBalance(s, function (err, balance) {
-            if (err) throw err
+            if (err) return cb(err)
             s.exchange.getQuote(s, function (err, quote) {
               if (err) throw err
               var bal = (s.product_id + ' Asset: ').grey + balance.asset.white + ' Currency: '.grey + balance.currency.yellow + ' Total: '.grey + n(balance.asset).multiply(quote.ask).add(balance.currency).value().toString().yellow
               console.log(bal)
-              process.exit()
+
+              if (so.calculate_currency) {
+                s.exchange.getQuote({'product_id': s.asset + '-' + so.calculate_currency}, function (err, asset_quote) {
+                  if (err)  throw err
+
+                  s.exchange.getQuote({'product_id': s.currency + '-' + so.calculate_currency}, function (err, currency_quote) {
+                    if (err)  throw err
+                    var asset_total = balance.asset * asset_quote.bid
+                    var currency_total = balance.currency * currency_quote.bid
+                    console.log((so.calculate_currency + ': ').grey + (asset_total + currency_total))
+
+                    process.exit()
+                  })
+                })
+              }
+              else {
+                process.exit()
+              }
             })
           })
         }
+
         balance()
       })
   }
