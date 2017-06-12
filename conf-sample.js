0f6602c58ae5d5e5af13351c5160b3fa556c553f
Eigil Bjorgum <eigilb@tb.tux.no>
2017-06-12 09:22:20 +0200

    Updated conf-sample.js

--- conf-sample.js
+++ conf-sample.js
@@ -51,6 +51,12 @@ c.bitfinex.secret = 'YOUR-SECRET'
 // May use 'exchange' or 'trading' wallet balances. However margin trading may not work...read the API documentation.
 c.bitfinex.wallet = 'exchange'
 
+// to enable Bitfinex trading, enter your API credentials:
+c.bitstamp.key = 'YOUR-API-KEY'
+c.bitstamp.secret = 'YOUR-SECRET'
+// A client ID is required on Bitstamp
+c.bitstamp.client_id = 'YOUR-CLIENT-ID'
+
 // Optional stop-order triggers:
 
 // sell if price drops below this % of bought price (0 to disable)
