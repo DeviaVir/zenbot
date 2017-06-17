bc57ff31e84c2107d01ad9c52616b0e415d0c277
Carlos Rodriguez <carlos@s8f.org>
2017-06-17 04:19:31 -0700

    fix sim day count when days = 0

--- commands/sim.js
+++ commands/sim.js
@@ -108,7 +108,7 @@ module.exports = function container (get, set, clear) {
           var buy_hold_profit = s.start_capital ? n(buy_hold).subtract(s.start_capital).divide(s.start_capital) : n(0)
           output_lines.push('buy hold: ' + buy_hold.format('0.00000000').yellow + ' (' + n(buy_hold_profit).format('0.00%') + ')')
           output_lines.push('vs. buy hold: ' + n(s.balance.currency).subtract(buy_hold).divide(buy_hold).format('0.00%').yellow)
-          output_lines.push(s.my_trades.length + ' trades over ' + s.day_count + ' days (avg ' + (s.day_count ? n(s.my_trades.length / s.day_count).format('0.00') : 0) + ' trades/day)')
+          output_lines.push(s.my_trades.length + ' trades over ' + s.day_count + ' days (avg ' + n(s.my_trades.length / s.day_count).format('0.00') + ' trades/day)')
           var last_buy
           var losses = 0, sells = 0
           s.my_trades.forEach(function (trade) {
