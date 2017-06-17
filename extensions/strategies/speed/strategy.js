fad5b812255fa8fe52bdfe6b081dababe60d0e1b
Carlos Rodriguez <carlos@s8f.org>
2017-06-16 20:05:36 -0700

    experiment with variable filter

--- extensions/strategies/speed/strategy.js
+++ extensions/strategies/speed/strategy.js
@@ -9,27 +9,37 @@ module.exports = function container (get, set, clear) {
     getOptions: function () {
       this.option('period', 'period length', String, '1m')
       this.option('min_periods', 'min. number of history periods', Number, 52)
-      this.option('lookback', 'lookback periods to judge trend speed', Number, 2)
-      this.option('buy_rate', 'buy when speed reaches this % change', Number, 0.9)
-      this.option('sell_rate', 'sell when speed reaches this % change', Number, -0.6)
+      this.option('speed_ema_periods', 'lookback periods to judge speed baseline', Number, 50)
+      this.option('speed_mult', 'multiply with avg speed to get trigger value', Number, 1.5)
     },
 
     calculate: function (s) {
-      if (s.lookback[s.options.lookback - 1]) {
-        s.period.speed = (s.period.close - s.lookback[s.options.lookback - 1].close) / s.lookback[s.options.lookback - 1].close * 100
+      if (s.lookback[1]) {
+        if (s.lookback[1].close < s.period.close) {
+          s.period.gain_speed = (s.period.close - s.lookback[1].close) / s.lookback[1].close * 100
+          s.period.loss_speed = 0
+        }
+        else {
+          s.period.gain_speed = 0
+          s.period.loss_speed = (s.period.close - s.lookback[1].close) / s.lookback[1].close * -100
+        }
+        if (s.lookback[s.options.speed_ema_periods + 1]) {
+          get('lib.ema')(s, 'gain_speed_avg', s.options.speed_ema_periods, 'gain_speed')
+          get('lib.ema')(s, 'loss_speed_avg', s.options.speed_ema_periods, 'loss_speed')
+        }
       }
     },
 
     onPeriod: function (s, cb) {
-      if (typeof s.period.speed === 'number') {
-        if (s.period.speed >= s.options.buy_rate) {
+      if (typeof s.period.gain_speed_avg === 'number') {
+        if (s.period.gain_speed >= s.period.gain_speed_avg * s.options.speed_mult) {
           if (s.trend !== 'up') {
             s.acted_on_trend = false
           }
           s.trend = 'up'
           s.signal = !s.acted_on_trend ? 'buy' : null
         }
-        else if (s.period.speed <= s.options.sell_rate) {
+        else if (s.period.loss_speed >= s.period.loss_speed_avg * s.options.speed_mult) {
           if (s.trend !== 'down') {
             s.acted_on_trend = false
           }
@@ -42,8 +52,15 @@ module.exports = function container (get, set, clear) {
 
     onReport: function (s) {
       var cols = []
-      if (typeof s.period.speed === 'number') {
-        cols.push(z(8, n(s.period.speed).format('0.0000'), ' ')[s.trend === 'up' ? 'green' : 'red'])
+      if (s.period.loss_speed > s.period.gain_speed) {
+        cols.push(z(8, n(s.period.loss_speed).format('0.0000'), ' ').red)
+      }
+      else {
+        cols.push(z(8, n(s.period.gain_speed).format('0.0000'), ' ').green)
+      }
+      if (typeof s.period.gain_speed_avg === 'number') {
+        cols.push(z(8, n(s.period.gain_speed_avg).format('0.0000'), ' ').grey)
+        cols.push(z(8, n(s.period.loss_speed_avg).format('0.0000'), ' ').grey)
       }
       return cols
     }
