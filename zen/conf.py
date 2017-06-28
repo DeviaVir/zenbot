e698dad7e677f6b266a5219eef5561c27d018f03
Salvo Torre <gelotus@gmail.com>
2017-06-28 15:18:51 +0200

    Implement auto backfill timer
    For activating append backfill at te end of commandline. Interval can be set in conf

--- zen/conf.py
+++ zen/conf.py
@@ -1,11 +1,18 @@
+# Don't touch these lines!!!
 import random
-
-# zenbot dir, ex. "/app/zenbot" for docker "/full/path/of/zenbot/dir" for linux envinronment
+runid=random.randint(1000,9999)
+# User configurable parameters:
+# -----------------------------------------------------------------------------------------
+# zenbot dir, ex. "app/zenbot" for docker "/full/path/of/zenbot/dir" for linux envinronment
 path = '/home/gecko/zenbot'
+
 # name of python executable
 pyexc = 'python3.6'
+
 # Autobackfill interval in seconds, min 600 secs -> 10 mins
-bkfint = 30
+bkfint = 1800
+
+# You can edit this, please respect the python dict syntax
 selectors = {
     'BTC-CUR': ['gdax.BTC-USD', 'gdax.BTC-EUR', 'gdax.BTC-GBP'],
     'ETH-BTC': ['gdax.ETH-BTC'],
@@ -13,11 +20,22 @@ selectors = {
     'ETH-USD': ['gdax.ETH-USD'],
     'ETH-CUR': ['gdax.ETH-USD','gdax.ETH-EUR'],
 }
+
+# Concurrent threads
 partitions=2
+
+# Swozny please explain these parameters
 selectivity = 0.3
 
-runid=random.randint(1000,9999)
+# Ditto
 sigma = 20
+
+# Ditto
 indpb  = 0.3
+
+# Ditto
 mutpb = 0.3
+
+# Ditto
 cxpb = 0.3
+
--- zen/main.py
+++ zen/main.py
@@ -1,17 +1,17 @@
 import sys
 from functools import partial
-
+from timer import ThreadTimer
 from deap.tools import cxTwoPoint, mutGaussian
 from scoop import shared
 from blessings import Terminal
-
-from conf import indpb, sigma, partitions, selectors, path
+from subprocess import run, DEVNULL
+from conf import indpb, sigma, partitions, selectors, path, bkfint
 from evaluation import evaluate_zen, Andividual
 from evolution import evolve
 
 term = Terminal()
 
-def main(instrument, days, popsize, strategy='trend_ema'):
+def main(instrument, days, popsize, strategy='trend_ema', nobf=None):
     print(term.blue("Starting evolution...."))
     evaluate = partial(evaluate_zen, days=days)
     print(term.blue("Evaluating ") + term.green(str(popsize)) + term.blue(" individuals over ")
@@ -26,6 +26,9 @@ def main(instrument, days, popsize, strategy='trend_ema'):
     res = evolve(evaluate, Andividual, popsize)
     return res
 
+def bkfcmd():
+    run(f"cd {path} && fab backfill_local:\"{INSTRUMENT},{TOTAL_DAYS}\"", shell=True, stdout=DEVNULL)
+    return
 
 if __name__ == '__main__':
     INSTRUMENT = sys.argv[1]
@@ -34,11 +37,32 @@ if __name__ == '__main__':
         popsize = int(sys.argv[3])
     except:
         popsize = 10
+    if popsize < 10:
+        popsize = 10
+        print(term.red("Minimum population size is 10, overriding..."))
     try:
         strategy = sys.argv[4]
     except:
         strategy = 'trend_ema'
-    print(term.red("MAKE SURE YOU RUN fab backfill_local:<days>"))
-    print(term.red("otherwise it's all crap"))
+    try:
+        nobf = sys.argv[5]
+        if nobf == "backfill":
+            bkf = True
+    except:
+        bkf = False
+    if bkf == True:
+        print(term.green("Auto backfilling is ON"))
+        print(term.green("Executing inital backfilling, please wait..."))
+        bkfcmd()
+        if bkfint < 600:
+            print(term.red("Minimum interval for auto backfilling is 600 secs, overriding..."))
+            bkfint = 600
+        print(term.green("Done"))
+        print(term.green(f"Auto backfilling interval is {bkfint} secs"))
+        ThreadTimer(bkfint, bkfcmd)
+    else:
+        print(term.red("You don\'t have enabled autobackfill,"))
+        print(term.red("MAKE SURE YOU RUN fab backfill_local:\"<selector>,<days>\""))
+        print(term.red("otherwise it's all crap"))
     res = main(INSTRUMENT, TOTAL_DAYS, popsize, strategy)
     print(res)
