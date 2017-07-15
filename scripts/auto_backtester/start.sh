2c185c290cc8c56c869665795ac1f4568216227c
TheRoboKitten <jacobcmcqueen@gmail.com>
2017-07-15 12:37:58 -0500

    Delete start.sh

--- scripts/auto_backtester/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-sudo rm -rf /zenbot4/scripts/auto_backtester/simulations
-sudo rm -rf /zenbot4/scripts/auto_backtester/backtesting*
-sudo mkdir /zenbot4/scripts/auto_backtester/simulations/
-sudo cp /zenbot4/scripts/auto_backtester/index.php.graph /zenbot4/scripts/auto_backtester/simulations/index.php
-cd /zenbot4/scripts/auto_backtester/
-sudo python trend_ema.py
