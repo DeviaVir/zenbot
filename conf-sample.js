48ee99e734ce892566294437dc4b2450f9391588
piedrogenius <piedro.genius@gmail.com>
2017-07-28 12:26:43 +0200

    xmpp for trading alarms (#333)
    * use of xmpp
    
    * xmpp configs
    
    * Update conf-sample.js
    
    * Update engine.js

--- conf-sample.js
+++ conf-sample.js
@@ -124,3 +124,23 @@ c.rsi_periods = 14
 c.balance_snapshot_period = '15m'
 // avg. amount of slippage to apply to sim trades
 c.avg_slippage_pct = 0.045
+
+//xmpp configs
+
+c.xmppon=0  // 0 xmpp disabled; 1 xmpp enabled (credentials should be correct)
+
+if (c.xmppon) {
+
+  c.xmpp = require('simple-xmpp');
+
+  c.xmpp.connect({
+                jid                    : 'trader@domain.com', //xmpp account trader bot
+                password               : 'Password',          //xmpp password
+                host                   : 'domain.com',        //xmpp domain
+                port                   : 5222                 //xmpp port
+  });
+
+  c.xmppto="MeMyselfAndI@domain.com" //xmpp alert to friend
+}
+//end xmpp configs
+
