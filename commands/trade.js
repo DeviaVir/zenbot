60d1759d6b0c761a050449644ef1d2487bfd9dcb
Clifford Roche <clifford.roche@gmail.com>
2017-08-12 23:22:29 -0400

    Fix typo

--- commands/trade.js
+++ commands/trade.js
@@ -20,7 +20,7 @@ module.exports = function container (get, set, clear) {
       .option('--order_type <type>', 'order type to use (maker/taker)', /^(maker|taker)$/i, c.order_type)
       .option('--paper', 'use paper trading mode (no real trades will take place)', Boolean, false)
       .option('--manual', 'watch price and account balance, but do not perform trades automatically', Boolean, false)
-      .option('--non-interactive', 'disable keyboard inputs to the bot', Boolean, false)
+      .option('--non_interactive', 'disable keyboard inputs to the bot', Boolean, false)
       .option('--currency_capital <amount>', 'for paper trading, amount of start capital in currency', Number, c.currency_capital)
       .option('--asset_capital <amount>', 'for paper trading, amount of start capital in asset', Number, c.asset_capital)
       .option('--avg_slippage_pct <pct>', 'avg. amount of slippage to apply to paper trades', Number, c.avg_slippage_pct)
