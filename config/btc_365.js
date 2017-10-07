72aa8083c3b01cfd30e55703211253265d998845
Corstiaan <ch@fluxmatix.com>
2017-10-07 20:49:37 +0200

    dump

---
+++ config/btc_365.js
@@ -0,0 +1,11 @@
+var c = module.exports = {}
+
+c.selector = 'gdax.BTC-EUR'
+c.strategy = 'macd'
+c.currency_capital = 1000
+c.overbought_rsi_periods = 14
+c.buy_pct = 100
+c.period = '2h'
+c.down_trend_threshold = 3
+c.order_type = 'maker'
+c.stats = true
