189ee640806ab2c69a8d3d04176588ffc9003795
Corstiaan <ch@fluxmatix.com>
2017-10-11 12:57:48 +0200

    dump

---
+++ config/dash_365.js
@@ -0,0 +1,10 @@
+var c = module.exports = {}
+
+c.selector = 'poloniex.DASH-BTC'
+c.strategy = 'sar'
+c.currency_capital = 0.1
+c.buy_pct = 100
+c.period = '4h'
+c.order_type = 'maker'
+c.stats = true
+c.sar_af = 0.01
