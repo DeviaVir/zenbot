99eccc9d1e6221ccc27c0a625421b41a20896ab2
Corstiaan <ch@fluxmatix.com>
2017-10-11 11:38:31 +0200

    dump

--- config/btc_90.js
+++ config/btc_90.js
@@ -1,11 +1,9 @@
 var c = module.exports = {}
 
 c.selector = 'gdax.BTC-EUR'
-c.strategy = 'macd'
+c.strategy = 'sar'
 c.currency_capital = 100
-c.overbought_rsi_periods = 14
 c.buy_pct = 100
-c.period = '2h'
-c.down_trend_threshold = 3
+c.period = '2m'
 c.order_type = 'maker'
 c.stats = true
