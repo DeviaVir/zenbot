0376134dce66d6c89cbbc25c65cdca52812e2723
Corstiaan <ch@fluxmatix.com>
2017-10-12 16:04:20 +0200

    dump

---
+++ config/nav_365.js
@@ -0,0 +1,10 @@
+var c = module.exports = {}
+
+c.selector = 'poloniex.NAV-BTC'
+c.strategy = 'trust_distrust'
+c.currency_capital = 100
+c.buy_pct = 100
+c.sell_pct = 100
+c.period = '4h'
+c.order_type = 'maker'
+c.stats = true
