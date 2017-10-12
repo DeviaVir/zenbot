9ec49cdb9d60273422879e048f174b8bb82449e9
Corstiaan <ch@fluxmatix.com>
2017-10-12 17:15:52 +0200

    dump

---
+++ config/game_365.js
@@ -0,0 +1,10 @@
+var c = module.exports = {}
+
+c.selector = 'poloniex.GAME-BTC'
+c.strategy = 'trust_distrust'
+c.currency_capital = 0.1
+c.buy_pct = 100
+c.sell_pct = 100
+c.period = '30m'
+c.order_type = 'maker'
+c.stats = true
