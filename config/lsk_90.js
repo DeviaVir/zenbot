112ac6cfd3812479574aba6c9cb0ca62eda907fe
Corstiaan <ch@fluxmatix.com>
2017-10-12 20:43:36 +0200

    dump

---
+++ config/lsk_90.js
@@ -0,0 +1,10 @@
+var c = module.exports = {}
+
+c.selector = 'poloniex.LSK-BTC'
+c.strategy = 'ta_macd'
+c.currency_capital = 0.1
+c.buy_pct = 100
+c.sell_pct = 100
+c.period = '2h'
+c.order_type = 'maker'
+c.stats = true
