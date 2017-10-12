d5533e8494b19fa93a0a8dd76f273420db3dfdd5
Corstiaan <ch@fluxmatix.com>
2017-10-12 16:08:26 +0200

    dump

--- config/nav_365.js
+++ config/nav_365.js
@@ -2,7 +2,7 @@ var c = module.exports = {}
 
 c.selector = 'poloniex.NAV-BTC'
 c.strategy = 'trust_distrust'
-c.currency_capital = 100
+c.currency_capital = 0.1
 c.buy_pct = 100
 c.sell_pct = 100
 c.period = '4h'
