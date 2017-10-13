732b8d13a9440542a313b3f192f66e1512ae5e14
Corstiaan <ch@fluxmatix.com>
2017-10-13 17:48:54 +0200

    dump

---
+++ config/eth_90.js
@@ -0,0 +1,11 @@
+var c = module.exports = {}
+
+c.selector = 'gdax.ETH-EUR'
+c.strategy = 'trend_ema'
+c.currency_capital = 100
+c.overbought_rsi_periods = 14
+c.buy_pct = 100
+c.sell_pct = 100
+c.period = '30m'
+c.order_type = 'maker'
+c.stats = true
