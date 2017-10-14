281bbeada13ccc82d97269d95e4bd34711ef0eed
Corstiaan <ch@fluxmatix.com>
2017-10-14 10:24:55 +0200

    dump

---
+++ config/ltc_btc_365.js
@@ -0,0 +1,11 @@
+var c = module.exports = {}
+
+c.selector = 'gdax.LTC-BTC'
+c.strategy = 'macd'
+c.currency_capital = 100
+c.overbought_rsi_periods = 14
+c.buy_pct = 100
+c.sell_pct = 100
+c.period = '1d'
+c.order_type = 'maker'
+c.stats = true
