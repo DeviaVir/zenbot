05f836c2bfce8312dab038d3d89d319e2d7de76a
Corstiaan <ch@fluxmatix.com>
2017-10-14 22:15:08 +0200

    dump

---
+++ config/ardr_90.js
@@ -0,0 +1,11 @@
+var c = module.exports = {}
+
+c.selector = 'poloniex.ARDR-BTC'
+c.strategy = 'ta_ema'
+c.currency_capital = 0.1
+c.overbought_rsi_periods = 14
+c.buy_pct = 100
+c.sell_pct = 100
+c.period = '2h'
+c.order_type = 'maker'
+c.stats = true
