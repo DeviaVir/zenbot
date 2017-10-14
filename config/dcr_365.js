17c0605f7230ed2e050be585b7476f0c35d9c65c
Corstiaan <ch@fluxmatix.com>
2017-10-14 22:56:06 +0200

    dump

---
+++ config/dcr_365.js
@@ -0,0 +1,11 @@
+var c = module.exports = {}
+
+c.selector = 'poloniex.DCR-BTC'
+c.strategy = 'sar'
+c.currency_capital = 0.1
+c.overbought_rsi_periods = 14
+c.buy_pct = 100
+c.sell_pct = 100
+c.period = '4h'
+c.order_type = 'maker'
+c.stats = true
