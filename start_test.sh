5f0134c501c03ef923131a10d1f495b8c178450f
Corstiaan <ch@fluxmatix.com>
2017-10-16 18:28:05 +0200

    dump

--- start_test.sh
+++ start_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Starting new screen session..."
 screen -d -m -S zenbotstest
-screen -S zenbotstest -X screen wicked
+screen -S zenbotstest -X screen -S wicked
 screen -S zenbotstest -p 1 -X cd ~/repos/zenbot
 screen -S zenbotstest -p 1 -X exec zenbot trade --conf config/btc_365.js --paper
