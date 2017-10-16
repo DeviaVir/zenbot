a533c5c3215e0f0aca260d7f6bcbf771b768d2d7
Corstiaan <ch@fluxmatix.com>
2017-10-16 18:06:19 +0200

    dump

--- start_test.sh
+++ start_test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "Starting new screen session..."
 screen -d -m -S zenbotstest
-screen -S zenbotstest -p $windowCount -X cd ~/repos/zenbot
-screen -S zenbotstest -p $windowCount -X exec zenbot trade --conf config/btc_365.js --paper
-screen -S zenbotstest -p $windowCount -X xdotool key ctrl+s
-screen -S zenbotstest -p $windowCount -X xdotool key shift+a
-screen -S zenbotstest -p $windowCount -X xdotool key BackSpace BackSpace BackSpace BackSpace
-screen -S zenbotstest -p $windowCount -X xdotool type "$file"
-screen -S zenbotstest -p $windowCount -X xdotool key Enter
+screen -S zenbotstest -p 0 -X cd ~/repos/zenbot
+screen -S zenbotstest -p 0 -X exec zenbot trade --conf config/btc_365.js --paper
+screen -S zenbotstest -p 0 -X xdotool key ctrl+s
+screen -S zenbotstest -p 0 -X xdotool key shift+a
+screen -S zenbotstest -p 0 -X xdotool key BackSpace BackSpace BackSpace BackSpace
+screen -S zenbotstest -p 0 -X xdotool type "$file"
+screen -S zenbotstest -p 0 -X xdotool key Enter
