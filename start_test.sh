62001e461a979281351b11b5365e4d1c42260413
Corstiaan <ch@fluxmatix.com>
2017-10-16 18:13:29 +0200

    dump

--- start_test.sh
+++ start_test.sh
@@ -2,9 +2,9 @@
 echo "Starting new screen session..."
 screen -d -m -S zenbotstest
 screen -S zenbotstest -X cd ~/repos/zenbot
-screen -S zenbotstest -X exec zenbot trade --conf config/btc_365.js --paper
 screen -S zenbotstest -X exec xdotool key ctrl+s
 screen -S zenbotstest -X exec xdotool key shift+a
 screen -S zenbotstest -X exec xdotool key BackSpace BackSpace BackSpace BackSpace
 screen -S zenbotstest -X exec xdotool type "$file"
 screen -S zenbotstest -X exec xdotool key Enter
+screen -S zenbotstest -X exec zenbot trade --conf config/btc_365.js --paper
