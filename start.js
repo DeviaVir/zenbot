25690e6a02cd059e977f0376eeb19f086ba7b2cd
Corstiaan <ch@fluxmatix.com>
2017-10-18 15:52:47 +0200

    dump

--- start.js
+++ start.js
@@ -2,18 +2,22 @@ var fs = require('fs');
 require('shelljs/global');
 var path = require('path');
 
-var screen = exec('screen -d -m -S zenbots', {silent:false}).output;
+console.log('Starting tmux session...')
+var tmuxSession = exec('tmux new-session -d -s zenbots', {silent:false}).output;
 
+console.log('Starting bots...')
  fs.readdir('config/', function(err, items) {
  	var counter = 0;
     for (var i=0; i<items.length; i++) {
     	if(path.extname(items[i]) === ".js") {
     		if(counter != 0) {
-    			var newWindow = exec('screen -S zenbots -X screen').output;    		
-    		}
-			console.log('Starting screen window for ' + items[i])
-		    var window = exec('screen -S zenbots -p ' + counter + ' -X cd ~/repos/zenbot').output;
-		    var trade = exec('screen -S zenbots -p ' + counter + ' -X exec zenbot trade --conf config/' + items[i]).output;
+    			var newWindow = exec('tmux new-window -t zenbots:' + counter + ' -n livebots').output;    		
+    		} else {
+                var newFirstWindow = exec('tmux rename-window -t zenbots:0 ' + items[i]).output;
+            }
+			console.log('Starting tmux window for ' + items[i])
+		    var start = exec('tmux send-keys "zenbot trade --conf config/' + items[i] + '" C-m').output;
+		    var rename = exec('tmux rename-window -t zenbots:' + counter + ' ' + items[i]).output;
 		    counter++;
     	}             
     }
