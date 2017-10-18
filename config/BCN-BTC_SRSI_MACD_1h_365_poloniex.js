857ff9dab0f3abf97c2105759c8e1898f8df7a3d
Corstiaan <ch@fluxmatix.com>
2017-10-18 17:11:22 +0200

    dump

---
+++ config/BCN-BTC_SRSI_MACD_1h_365_poloniex.js
@@ -0,0 +1,12 @@
+var c = module.exports = {}
+
+c.selector = 'poloniex.ARDR-BTC'
+c.strategy = 'srsi_macd'
+c.currency_capital = 0.1
+c.overbought_rsi_periods = 14
+c.buy_pct = 100
+c.sell_pct = 100
+c.period = '1h'
+c.order_type = 'maker'
+c.stats = true
+c.mode = 'paper'
--- start.js
+++ start.js
@@ -10,14 +10,15 @@ console.log('Starting bots...')
  	var counter = 0;
     for (var i=0; i<items.length; i++) {
     	if(path.extname(items[i]) === ".js") {
+            var botName = items[i].slice(0, -3);
     		if(counter != 0) {
     			var newWindow = exec('tmux new-window -t zenbots:' + counter + ' -n livebots').output;    		
     		} else {
-                var newFirstWindow = exec('tmux rename-window -t zenbots:0 ' + items[i]).output;
+                var newFirstWindow = exec('tmux rename-window -t zenbots:0 ' + botName).output;
             }
-			console.log('Starting tmux window for ' + items[i])
+			console.log('Starting bot ' + items[i])
 		    var start = exec('tmux send-keys "zenbot trade --conf config/' + items[i] + '" C-m').output;
-		    var rename = exec('tmux rename-window -t zenbots:' + counter + ' ' + items[i]).output;
+		    var rename = exec('tmux rename-window -t zenbots:' + counter + ' ' + botName).output;
 		    counter++;
     	}             
     }
