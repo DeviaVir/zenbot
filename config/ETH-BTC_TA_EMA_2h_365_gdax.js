cae1db3e6beb47520a511d86a9feb91abcacce66
Corstiaan <ch@fluxmatix.com>
2017-10-20 22:32:17 +0200

    dump

---
+++ config/ETH-BTC_TA_EMA_2h_365_gdax.js
@@ -0,0 +1,12 @@
+var c = module.exports = {}
+
+c.selector = 'gdax.ETH-BTC'
+c.strategy = 'ta_ema'
+c.currency_capital = 100
+c.overbought_rsi_periods = 14
+c.buy_pct = 100
+c.sell_pct = 100
+c.period = '2h'
+c.order_type = 'maker'
+c.stats = true
+c.mode = 'paper'
