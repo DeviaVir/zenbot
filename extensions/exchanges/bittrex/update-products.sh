c2f875a779a263d0863b410e73ae924ef2bfa204
Jerry Tunin <jerrytunin@gmail.com>
2017-10-23 17:04:22 -0500

    Pulls min_size from Bittrex instead of being a static value.  Creates a static value for min_total of 50k Satoshi (0.005BTC), Bittrex's minimum order size.

--- extensions/exchanges/bittrex/update-products.sh
+++ extensions/exchanges/bittrex/update-products.sh
@@ -5,11 +5,12 @@ var bittrex = require('node.bittrex.api')
 var mapping
 var products = []
 
-function addProduct(base, quote, altname) {
+function addProduct(base, quote, minSize, altname) {
     products.push({
         asset: quote,
         currency: base,
-        min_size: '0.01',
+        min_size: minSize,
+        min_total: '0.0005',
         max_size: '1000000',
         increment: '0.00000001',
         label: base + '/' + quote
@@ -32,7 +33,7 @@ bittrex.getmarkets(function (data) {
         mapping = data.result
 
         Object.keys(data.result).forEach(function (result) {
-            addProduct(data.result[result].BaseCurrency, data.result[result].MarketCurrency, data.result[result].altname)
+            addProduct(data.result[result].BaseCurrency, data.result[result].MarketCurrency, data.result[result].MinTradeSize.toFixed(8), data.result[result].altname)
         })
         var target = require('path').resolve(__dirname, 'products.json')
         require('fs').writeFileSync(target, JSON.stringify(products, null, 2))
