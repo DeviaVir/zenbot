4355c8d21be01f557cfee98a931d742e55f74977
nedievas <nedievas@outlook.com>
2017-10-25 02:02:53 +0300

    HitBTC code cleaning (#633)
    removing defined values but never used

--- extensions/exchanges/hitbtc/exchange.js
+++ extensions/exchanges/hitbtc/exchange.js
@@ -1,23 +1,13 @@
-const ccxt = require ('ccxt'),
-  path = require('path'),
-  minimist = require('minimist'),
-  moment = require('moment'),
-  colors = require('colors'),
-  n = require('numbro')
+const ccxt = require('ccxt')
+var path = require('path')
 
-module.exports = function container(get, set, clear) {
+module.exports = function container (get, set, clear) {
   var c = get('conf')
-  var s = {
-    options: minimist(process.argv)
-  }
-  var so = s.options
-
-  var shownWarnings = false
 
   var public_client, authed_client
 
-  function publicClient() {
-    if (!public_client) public_client = new ccxt.hitbtc ({'apiKey': '', 'secret': '' })
+  function publicClient () {
+    if (!public_client) public_client = new ccxt.hitbtc({ 'apiKey': '', 'secret': '' })
     return public_client
   }
 
@@ -26,18 +16,16 @@ module.exports = function container(get, set, clear) {
       if (!c.hitbtc || !c.hitbtc.key || !c.hitbtc.key === 'YOUR-API-KEY') {
         throw new Error('please configure your HitBTC credentials in ' + path.resolve(__dirname, 'conf.js'))
       }
-
-      authed_client = new ccxt.hitbtc ({ 'apiKey': c.hitbtc.key,'secret': c.hitbtc.secret })
+      authed_client = new ccxt.hitbtc({ 'apiKey': c.hitbtc.key, 'secret': c.hitbtc.secret })
     }
     return authed_client
   }
-
 
   function joinProduct(product_id) {
     return product_id.split('-')[0] + '/' + product_id.split('-')[1]
   }
 
-  function retry(method, args) {
+  function retry (method, args) {
     if (method !== 'getTrades') {
       console.error(('\n HitBTC API is down! unable to call ' + method + ', retrying in 10s').red)
     }
@@ -62,25 +50,24 @@ module.exports = function container(get, set, clear) {
       var func_args = [].slice.call(arguments)
       var args = {
         id: joinProduct(opts.product_id),
-        'side' : true,
-        'by' : 'ts',
+        'side': true,
+        'by': 'ts'
       }
       if (opts.from) {
         args.from = opts.from
       }
-      if(opts.to){
+      if (opts.to) {
         args.till = opts.to
       }
-
       var client = publicClient()
       client.fetchTrades(joinProduct(opts.product_id), args).then(result => {
-        var trades = result.map(function(trade) {
+        var trades = result.map(function (trade) {
           return {
             trade_id: trade.id,
             time: trade.timestamp,
             size: parseFloat(trade.amount),
             price: parseFloat(trade.price),
-            side: trade.side,
+            side: trade.side
           }
         })
         cb(null, trades)
@@ -94,14 +81,14 @@ module.exports = function container(get, set, clear) {
     getBalance: function (opts, cb) {
       var func_args = [].slice.call(arguments)
       var client = authedClient()
-      client.fetchBalance().then(result =>{
+      client.fetchBalance().then(result => {
         var balance = {asset: 0, currency: 0}
-        Object.keys(result).forEach(function(key){
-          if(key === opts.currency){
+        Object.keys(result).forEach(function (key) {
+          if (key === opts.currency) {
             balance.currency = result[key].free
             balance.currency_hold = result[key].used
           }
-          if(key === opts.asset){
+          if (key === opts.asset) {
             balance.asset = result[key].free
             balance.asset_hold = result[key].used
           }
@@ -114,11 +101,10 @@ module.exports = function container(get, set, clear) {
         })
     },
 
-
     getQuote: function (opts, cb) {
       var func_args = [].slice.call(arguments)
       var client = publicClient()
-      client.fetchTicker(joinProduct(opts.product_id)).then(result =>{
+      client.fetchTicker(joinProduct(opts.product_id)).then(result => {
         cb(null, { bid: result.bid, ask: result.ask })
       })
         .catch(function (error) {
@@ -151,7 +137,7 @@ module.exports = function container(get, set, clear) {
       }
       opts.side = 'buy'
       delete opts.order_type
-      client.createOrder(opts.market, opts.type, opts.side, opts.amount, opts.price, opts).then(result =>{
+      client.createOrder(opts.market, opts.type, opts.side, opts.amount, opts.price, opts).then(result => {
         if (result && result.message === 'Insufficient funds') {
           var order = {
             status: 'rejected',
@@ -159,7 +145,6 @@ module.exports = function container(get, set, clear) {
           }
           return cb(null, order)
         }
-
         orders['~' + result.id] = result
         cb(null, result)
       }).catch(function (error) {
@@ -181,7 +166,7 @@ module.exports = function container(get, set, clear) {
       }
       opts.side = 'sell'
       delete opts.order_type
-      client.createOrder(opts.market, opts.type, opts.side, opts.amount, opts.price, opts).then(result =>{
+      client.createOrder(opts.market, opts.type, opts.side, opts.amount, opts.price, opts).then(result => {
         if (result && result.message === 'Insufficient funds') {
           var order = {
             status: 'rejected',
@@ -189,7 +174,6 @@ module.exports = function container(get, set, clear) {
           }
           return cb(null, order)
         }
-
         orders['~' + result.id] = result
         cb(null, result)
       }).catch(function (error) {
@@ -202,7 +186,6 @@ module.exports = function container(get, set, clear) {
       var func_args = [].slice.call(arguments)
       var client = authedClient()
       client.getOrder(opts.order_id, function (err, resp, body) {
-
         if (err) return retry('getOrder', func_args, err)
         if (resp.statusCode === 404) {
           // order was cancelled. recall from cache
@@ -216,8 +199,7 @@ module.exports = function container(get, set, clear) {
 
     getCursor: function (trade) {
       return (trade.time || trade)
-    },
+    }
   }
   return exchange
 }
-
