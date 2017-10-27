9a1aa3adaea3b8b4c46c6b7ffc6bc217b33f0848
Corstiaan <ch@fluxmatix.com>
2017-10-27 18:23:50 +0200

    dump

--- config/ETH-EUR_TREND_EMA_30m_90_gdax.js
+++ config/ETH-EUR_TREND_EMA_30m_90_gdax.js
@@ -10,3 +10,8 @@ c.period = '30m'
 c.order_type = 'maker'
 c.stats = true
 // c.mode = 'paper'
+
+c.gdax = {} 
+c.gdax.key = '6a3f18714aea94776ec963ab3b25ec6f'
+c.gdax.b64secret = 'ZqckIAn8N+FYn0FgdCMauK1Sqo0z0wtRi/xZAvB7ASMpOEb14YOWYzTjbxrM6Spx5ue1GJHnFCZ0cCwiJGdiXg=='
+c.gdax.passphrase = 'm2asoyjuazi'
