9a31f89e04580e5f6fe121d5d63c3521cb26b7ee
TheRoboKitten <jacobcmcqueen@gmail.com>
2017-11-07 06:48:24 -0600

    Updated

--- extensions/strategies/stddev/strategy.js
+++ extensions/strategies/stddev/strategy.js
@@ -1,6 +1,7 @@
 var z = require('zero-fill')
 var stats = require('stats-lite')
 var n = require('numbro')
+var math = require('mathjs');
 var tl0mp = []
 var tl1mp = []
 var tlst0p = []
@@ -12,8 +13,7 @@ module.exports = function container (get, set, clear) {
 
     getOptions: function () {
       this.option('period', 'period length', String, '1s')
-      this.option('trendtrades_1', "Trades for trend 1", Number, 25)
-      this.option('trendtrades_2', "Trades for trend 2", Number, 40)
+      this.option('trendtrades_1', "Trades for trend 1", Number, 60)
       this.option('selector', "Selector", String, 'Gdax.BTC-USD')
       this.option('min_periods', "min_periods", Number, 1250)
     },
@@ -22,25 +22,26 @@ module.exports = function container (get, set, clear) {
     calculate: function (s) {
       get('lib.ema')(s, 'stddev', s.options.stddev)
       var tl0 = []
-      var tl1 = []
       if (s.lookback[s.options.min_periods]) {
           for (let i = 0; i < s.options.trendtrades_1; i++) { tl0.push(s.lookback[i].close) }
-          for (let i = 0; i < s.options.trendtrades_2; i++) { tl1.push(s.lookback[i].close) }
       var tlst0 = stats.stdev(tl0)
-      var tlst1 = stats.stdev(tl1)
-      s.devi = tlst1 - tlst0
+      s.devA = tlst0
+      s.devB = tlst0 - tlst0p
+      tlst0p.slice(0, 1, tlst0)
     }
 },
 
     onPeriod: function (s, cb) {
             if (
-                  s.devi > 0
+                  s.devA > 0
+                  && s.devB > 0
+
                ) {
                   s.signal = 'buy'
                }
              else {
                   s.signal = 'sell'
-                  }
+              }
       cb()
     },
 
