cecca544d3b4891abdd7167464d6973572d8c922
TheRoboKitten <jacobcmcqueen@gmail.com>
2017-11-07 07:54:08 -0600

    Update strategy.js

--- extensions/strategies/stddev/strategy.js
+++ extensions/strategies/stddev/strategy.js
@@ -3,38 +3,34 @@ var stats = require('stats-lite')
 var n = require('numbro')
 var math = require('mathjs');
 var tl0mp = []
-var tl1mp = []
-var tlst0p = []
-var tlst1p = []
 module.exports = function container (get, set, clear) {
   return {
     name: 'stddev',
     description: 'Trade when % change from last two 1m periods is higher than average.',
 
     getOptions: function () {
-      this.option('period', 'period length', String, '1s')
-      this.option('trendtrades_1', "Trades for trend 1", Number, 60)
+      this.option('period', 'period length', String, '100ms')
+      this.option('trendtrades_1', "Trades for trend 1", Number, 2500)
       this.option('selector', "Selector", String, 'Gdax.BTC-USD')
-      this.option('min_periods', "min_periods", Number, 1250)
+      this.option('min_periods', "min_periods", Number, 3000)
     },
 
 
     calculate: function (s) {
       get('lib.ema')(s, 'stddev', s.options.stddev)
       var tl0 = []
-      if (s.lookback[s.options.min_periods]) {
+      if (s.lookback[s.options.trendtrades_1]) {
           for (let i = 0; i < s.options.trendtrades_1; i++) { tl0.push(s.lookback[i].close) }
-      var tlst0 = stats.stdev(tl0)
-      s.devA = tlst0
-      s.devB = tlst0 - tlst0p
-      tlst0p.slice(0, 1, tlst0)
+      s.tlst0 = stats.stdev(tl0)
+      s.mean = math.mean(tl0)
+      s.sign = s.mean - s.tlst0
+      s.sig = tl0[0] - s.sign
     }
 },
 
     onPeriod: function (s, cb) {
             if (
-                  s.devA > 0
-                  && s.devB > 0
+                  s.sig > 0
 
                ) {
                   s.signal = 'buy'
