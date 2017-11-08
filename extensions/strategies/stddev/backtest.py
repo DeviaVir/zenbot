c89c7058d373df9f692d795a517a896299320eda
TheRoboKitten <jacobcmcqueen@gmail.com>
2017-11-07 22:14:08 -0600

    Update backtest.py

--- extensions/strategies/stddev/backtest.py
+++ extensions/strategies/stddev/backtest.py
@@ -4,8 +4,10 @@ y = 10
 while (x < 1000):
     y = x
     while (y < 1000):
-        command = 'sudo ./zenbot.sh sim --strategy=stddev --trendtrades_1=' + x + ' --trendtrades_2=' + y + ' --min_periods=1250 --period=100ms --days=3'
-        call(command, shell=true)
+        A = str(x)
+        B = str(y)
+        command = 'sudo ./zenbot.sh sim --strategy=stddev --trendtrades_1=' + A + ' --trendtrades_2=' + B + ' --min_periods=1250 --period=100ms --days=3'
+        call(command, shell=True)
         print(x)
         print(y)
         y + 10
