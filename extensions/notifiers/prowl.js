640287adf16c608a15c4410b143e94a56bb887c9
Bryan Berger <bryan@bryanberger.com>
2017-11-08 07:40:31 -0500

    Removed unused variable: remaining. Included package-lock.json

--- extensions/notifiers/prowl.js
+++ extensions/notifiers/prowl.js
@@ -4,7 +4,7 @@ module.exports = function container (get, set, clear) {
   var prowl = {
     pushMessage: function(config, title, message) {
       var p = new Prowl(config.key)
-      p.push(message, title, function(err, remaining) {
+      p.push(message, title, function(err) {
         if (err) {
           console.log('error: Push message failed, ' + err)
           return
