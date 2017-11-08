97b69a4634dd2f2c4f1e6eb6696879ae121e3246
TheRoboKitten <jacobcmcqueen@gmail.com>
2017-11-08 01:01:17 -0600

    Note the y*6 + 5 at bottom

--- extensions/strategies/stddev/backtest.py
+++ extensions/strategies/stddev/backtest.py
@@ -1,22 +1,22 @@
 from subprocess import call
-x = 10
-y = 10
+x = 5 
+y = 5 
 
 while (x < 1000):
     while (y < 1000):
         A = str(x)
         B =  y
         D = B * 0.333333333333333333333333333333333333333333333333333333333333333333333333
-        E = D - B
+        E = B - D
         F = B + D
         H = int(round(F))
         L = int(round(E))
-        while (L < H):
+        while (H > L):
             I = str(L)
             command = 'sudo ./zenbot.sh sim --strategy=stddev --trendtrades_1=' + A + ' --trendtrades_2=' + I + ' --min_periods=1250 --period=100ms --days=3'
             print(command) 
             call(command, shell=True)
             L = L + 1
-    y = (y + 1) * 6
-    x = x + 1
+    y = (y + 5) * 6
+    x = x + 5
     y = x
