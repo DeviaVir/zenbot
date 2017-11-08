6473033168b474e3ea7b7a8f80936c802cdbd960
TheRoboKitten <jacobcmcqueen@gmail.com>
2017-11-08 00:53:39 -0600

    Update backtest.py

--- extensions/strategies/stddev/backtest.py
+++ extensions/strategies/stddev/backtest.py
@@ -1,22 +1,22 @@
 from subprocess import call
-x = 5
-y = 5
+x = 10
+y = 10
 
 while (x < 1000):
     while (y < 1000):
         A = str(x)
-        B =  y * 3
-        D = B * 0.3333333333333333333333333333333333333333
+        B =  y
+        D = B * 0.333333333333333333333333333333333333333333333333333333333333333333333333
         E = D - B
         F = B + D
-        G = round(E)
-        H = round(F)
-        while (G < H)
-             I = str(G)
-             command = 'sudo ./zenbot.sh sim --strategy=stddev --trendtrades_1=' + A + ' --trendtrades_2=' + I + ' --min_periods=1250 --period=100ms --days=3'
-        print(command) 
-        call(command, shell=True)
-        G = G + 1
-        y = y + 5
+        H = int(round(F))
+        L = int(round(E))
+        while (L < H):
+            I = str(L)
+            command = 'sudo ./zenbot.sh sim --strategy=stddev --trendtrades_1=' + A + ' --trendtrades_2=' + I + ' --min_periods=1250 --period=100ms --days=3'
+            print(command) 
+            call(command, shell=True)
+            L = L + 1
+    y = (y + 1) * 6
     x = x + 1
     y = x
