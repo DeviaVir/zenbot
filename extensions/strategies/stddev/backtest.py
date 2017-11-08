1d886a5a5f0d55cc017746e379ff5a7601f32a83
TheRoboKitten <jacobcmcqueen@gmail.com>
2017-11-07 23:07:25 -0600

    Tune sell order first, then buy order if trend/graph starts on UP signal.
    Tune sell order first, then buy order if trend/graph starts on UP signal.
    Look at the graph and results, check when it buys and sells.
    
    If signal is bought in beginning (on up) you need to make sure the sell signal is hit as soon as it becomes the first amount of sensitive to tune the standard deviation sell order.

--- extensions/strategies/stddev/backtest.py
+++ extensions/strategies/stddev/backtest.py
@@ -2,13 +2,13 @@ from subprocess import call
 x = 10
 y = 10
 
-while (x < 1000):
-    while (y < 1000):
+while (y < 1000):
+    while (x < 1000):
         A = str(x)
         B = str(y)
-        command = 'sudo ./zenbot.sh sim --strategy=stddev --trendtrades_1=' + A + ' --trendtrades_2=' + B + ' --min_periods=1250 --period=100ms --days=3'
+        command = 'sudo ./zenbot.sh sim --strategy=stddev --trendtrades_1=' + A + ' --trendtrades_2=' + B + ' --min_periods=1250 --period=100ms --days$
         print(command)
         call(command, shell=True)
-        y = y + 10
-    x = x + 10
+        x = x + 10
+    y = y + 10
     y = x
