19b21f70046e15f877ecc86839ac290136d7b0b1
TheRoboKitten <jacobcmcqueen@gmail.com>
2017-11-08 19:54:29 -0600

    Update strategy.js

--- extensions/strategies/trendline/strategy.js
+++ extensions/strategies/trendline/strategy.js
@@ -1,3 +1,4 @@
+#./zenbot.sh sim --strategy=stddev --period=10s --days=7
 var np = require('numjs')
 var z = require('zero-fill')
 var stats = require('stats-lite')
@@ -6,16 +7,16 @@ var math = require('mathjs')
 var trend = require('trend')
 module.exports = function container (get, set, clear) {
   return {
-    name: 'stddev',
+    name: 'trendline',
     description: 'Trade when % change from last two 1m periods is higher than average.',
     getOptions: function () {
       this.option('period', 'period length', String, '10s')
-      this.option('trendtrades_1', "Trades to learn from", Number, 50)
+      this.option('trendtrades_1', "Doesnt really matter, will fix later", Number, 55)
       this.option('selector', "Selector", String, 'Gdax.BTC-USD')
       this.option('min_periods', "min_periods", Number, 1250)
     },
     calculate: function (s) {
-      get('lib.ema')(s, 'neural', s.options.neural)
+      get('lib.ema')(s, 'trendline', s.options.trendline)
       var tl1 = []
       if (s.lookback[s.options.min_periods]) {
           for (let i = 0; i < s.options.trendtrades_1; i++) { tl1.push(s.lookback[i].close) }
