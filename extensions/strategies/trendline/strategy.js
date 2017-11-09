a5a0991726cfd6260da47fa268220ee915aa6aa0
TheRoboKitten <jacobcmcqueen@gmail.com>
2017-11-08 19:51:36 -0600

    Updated trendline

--- extensions/strategies/trendline/strategy.js
+++ extensions/strategies/trendline/strategy.js
@@ -1,18 +1,21 @@
+var np = require('numjs')
+var z = require('zero-fill')
 var stats = require('stats-lite')
+var n = require('numbro')
+var math = require('mathjs')
 var trend = require('trend')
-var oldval = []
 module.exports = function container (get, set, clear) {
   return {
-    name: 'trendline',
+    name: 'stddev',
     description: 'Trade when % change from last two 1m periods is higher than average.',
     getOptions: function () {
-      this.option('period', 'period length', String, '1s')
-      this.option('trendtrades_1', "Trades - doesnt really matter ATM", Number, 55)
+      this.option('period', 'period length', String, '10s')
+      this.option('trendtrades_1', "Trades to learn from", Number, 50)
       this.option('selector', "Selector", String, 'Gdax.BTC-USD')
-      this.option('min_periods', "min_periods", Number, 100)
+      this.option('min_periods', "min_periods", Number, 1250)
     },
     calculate: function (s) {
-      get('lib.ema')(s, 'trendline', s.options.trendline)
+      get('lib.ema')(s, 'neural', s.options.neural)
       var tl1 = []
       if (s.lookback[s.options.min_periods]) {
           for (let i = 0; i < s.options.trendtrades_1; i++) { tl1.push(s.lookback[i].close) }
@@ -22,22 +25,20 @@ module.exports = function container (get, set, clear) {
           growth = trend(chart, {
               lastPoints: 3,
               avgPoints: 53,
-              avgMinimum: 53,
-              reversed: true 
-         });
-         s.diff = growth - oldval[0]
-         oldval.slice(0, 1, growth)
+              avgMinimum: 10,
+              reversed: true
+         }),
          s.growth = growth
   }
 },
     onPeriod: function (s, cb) {
             if (
-                  (s.diff < 0)
+                  (s.growth < 1.0006)
                ) {
                   s.signal = 'sell'
               }
             else if (
-                  (s.growth > 1.000000001)
+                  (s.growth >  1.0006)
                ) {
                   s.signal = 'buy'
                }
