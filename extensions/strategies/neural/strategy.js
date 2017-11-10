8bf2d7ff33b0f56a46604ad74c423fba187bd4ef
TheRoboKitten <jacobcmcqueen@gmail.com>
2017-11-10 04:27:34 -0600

    Faster code, more correct

--- extensions/strategies/neural/strategy.js
+++ extensions/strategies/neural/strategy.js
@@ -9,24 +9,19 @@ module.exports = function container (get, set, clear) {
     description: 'Use neural learning to predict future price. Starts when min_period lasts longer than backfill.',
     getOptions: function () {
       this.option('period', 'period length - make sure to lower your poll trades time to lower than this value', String, '5s')
-      this.option('trendtrades_1', "Trades to learn from and calculate mean from. (predixtion - mean > 0 = buy)", Number, 1000)
-      this.option('trains', "Don't change this / N/A", Number, 1)
+      this.option('trendtrades_1', "Trades to learn from and calculate mean from. (predixtion - mean > 0 = buy)", Number, 50)
       this.option('activation_1_type', "Neuron Activation Type: sigmoid, tanh, relu", String, 'sigmoid')
-      this.option('neurons_1', "Neurons in layer 1", Number, 50)
-      this.option('activation_2_type', "Neuron Activation Type: sigmoid, tanh, relu", String, 'sigmoid')
-      this.option('neurons_2', "Neurons in layer 2", Number, 50)
-      this.option('activation_3_type', "Neuron Activation Type: sigmoid, tanh, relu", String, 'sigmoid')
-      this.option('neurons_3', "Neurons in layer 3", Number, 50)
-      this.option('depth', "Don't change this / N/A", Number, 1)
+      this.option('neurons_1', "Neurons in layer 1", Number, 20)
+      this.option('depth', "Rows of data to predict ahead for matches/learning", Number, 9)
       this.option('selector', "Selector", String, 'Gdax.BTC-USD')
-      this.option('min_periods', "Set this to same as trendtrades_1", Number, 1000)
+      this.option('min_periods', "Set this to same as trendtrades_1", Number, 9)
       this.option('start_trigger', "Minimum trades to start calculating after x trades load", Number, 1500)
     },
     calculate: function (s) {
       get('lib.ema')(s, 'neural', s.options.neural)
       var tl1 = []
       // Soemething needs to be done about this line below, s.lookback.length is always too early.
-      if (s.lookback.length > s.options.start_trigger) {
+      if (s.lookback[s.options.start_trigger]) {
           for (let i = 0; i < s.options.trendtrades_1; i++) { tl1.push(s.lookback[i].close) }
           // create a net out of it
           var net = new convnetjs.Net();
@@ -34,36 +29,41 @@ module.exports = function container (get, set, clear) {
           var layer_defs = [];
           layer_defs.push({type:'input', out_sx:1, out_sy:1, out_depth:d});
           layer_defs.push({type:'fc', num_neurons:s.options.neurons_1, activation:s.options.activation_1_type});
-          layer_defs.push({type:'fc', num_neurons:s.options.neurons_2, activation:s.options.activation_2_type});
-          layer_defs.push({type:'fc', num_neurons:s.options.neurons_3, activation:s.options.activation_3_type});
           layer_defs.push({type:'regression', num_neurons:1});
           var net = new convnetjs.Net();
           net.makeLayers(layer_defs);
-          var my_data = tl1.reverse()
-          var trainer = new convnetjs.SGDTrainer(net, {learning_rate:0.01, momentum:0.2, batch_size:1, l2_decay:0.001});
+          var my_data = tl1
+          var trainer = new convnetjs.SGDTrainer(net, {learning_rate:0.01, momentum:0.0, batch_size:1, l2_decay:0.001});
 
           var learn = function () {
-             future = []
-             for(var j = 0; j < s.options.trains; j++) {
-                 for(var i = 0; i < my_data.length - d; i++) {
-                 var data = my_data.slice(i, i + d);
-                 var real_value = [my_data[i + d]];
-                 var x = new convnetjs.Vol(data);
-                 trainer.train(x, real_value);
-                 var predicted_values = net.forward(x);
+             for(var j = 0; j < 100; j++){
+                 for (var i = 0; i < my_data.length - d; i++) {
+                   var data = my_data.slice(i, i + d);
+                   var real_value = [my_data[i + d]];
+                   var x = new convnetjs.Vol(data);
+                   trainer.train(x, real_value);
+                   var predicted_values = net.forward(x);
                  }
-               future.push(predicted_values.w[0])
              }
-           }
-        learn();
-        s.output = math.mean(future)
-        s.sig = s.output - math.mean(tl1)
-      }
+         }
+          var predict = function(data){
+            var x = new convnetjs.Vol(data);
+            var predicted_value = net.forward(x);
+            return predicted_value.w[0];
+          }
+
+         learn();
+         var item = tl1;
+         s.prediction = predict(item)
+         s.sig = s.prediction - tl1[0]
+        }
     },
+
 
     onPeriod: function (s, cb) {
         if (
-            s.sig > 0
+            // for some reason I swapped this
+            s.sig < 0
            )
            {
             s.signal = 'buy'
