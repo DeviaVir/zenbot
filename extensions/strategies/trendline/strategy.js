2df3db368fd31bf7631206e19228a985f895fce0
darthyvader <34537029+darthyvader@users.noreply.github.com>
2017-12-14 02:53:02 -0600

    Update strategy.js

--- extensions/strategies/trendline/strategy.js
+++ extensions/strategies/trendline/strategy.js
@@ -11,7 +11,7 @@ module.exports = function container (get, set, clear) {
       this.option('period', 'period length', String, '1s')
       this.option('lastpoints', "Number of trades for short trend average", Number, 100)
       this.option('avgpoints', "Number of trades for long trend average", Number, 1000)
-	    this.option('lastpoints2', "Number of trades for short trend average", Number, 10)
+      this.option('lastpoints2', "Number of trades for short trend average", Number, 10)
       this.option('avgpoints2', "Number of trades for long trend average", Number, 100)
       this.option('min_periods', "Basically avgpoints + a BUNCH of more preroll periods for anything less than 5s period", Number, 5000)
       this.option('max_sell_loss_pct', "Max Sell loss Pct", Number, 0)
@@ -61,11 +61,11 @@ module.exports = function container (get, set, clear) {
     },
     onReport: function (s) {
       var cols = []
-	    cols.push('  ')
+      cols.push('  ')
       cols.push(z(8, n(s.stats).format('0.00000'), ' ')[s.stats > 1 ? 'green' : 'red'])
-	    cols.push('  ')
-	    cols.push(z(8, n(s.stats2).format('0.00000'), ' ')[s.stats2 > 1 ? 'green' : 'red'])
-	    cols.push('  ')
+      cols.push('  ')
+      cols.push(z(8, n(s.stats2).format('0.00000'), ' ')[s.stats2 > 1 ? 'green' : 'red'])
+      cols.push('  ')
       return cols
     },
   }
