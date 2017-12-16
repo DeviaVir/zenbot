86696706bba2419fa8f0c8db509b613756eafb98
darthyvader <34537029+darthyvader@users.noreply.github.com>
2017-12-15 19:34:55 -0600

    (I think my theory is confirmed about the preroll-flood.
    (I think my theory is confirmed about the preroll-flood.

--- extensions/strategies/trendline/strategy.js
+++ extensions/strategies/trendline/strategy.js
@@ -13,7 +13,7 @@ module.exports = function container (get, set, clear) {
       this.option('avgpoints', "Number of trades for long trend average", Number, 100)
       this.option('lastpoints2', "Number of trades for short trend average", Number, 5)
       this.option('avgpoints2', "Number of trades for long trend average", Number, 50)
-      this.option('min_periods', "Basically avgpoints + a BUNCH of more preroll periods for anything less than 5s period", Number, 5000)
+      this.option('min_periods', "Basically avgpoints + a BUNCH of more preroll periods for anything less than 5s period", Number, 1000)
       this.option('max_sell_loss_pct', "Max Sell loss Pct", Number, 0)
 
     },
