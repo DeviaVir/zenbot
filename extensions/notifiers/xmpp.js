781e89923cec1245db0834a0241eae85c1131bbd
defkev <defkev@gmail.com>
2017-12-17 14:50:22 +0100

    Improve XMPP reliability (#894)
    Only connect once, keep the connection open and reconnect if necessary.

--- extensions/notifiers/xmpp.js
+++ extensions/notifiers/xmpp.js
@@ -3,16 +3,17 @@ var simplexmpp = require('simple-xmpp')
 module.exports = function container (get, set, clear) {
   var xmpp = {
     pushMessage: function(config, title, message) {
-      simplexmpp.connect({
-        jid      : config.jid,
-        password : config.password,
-        host     : config.host,
-        port     : config.port
-      })
+      if (!simplexmpp.conn) {
+        simplexmpp.connect({
+          jid       : config.jid,
+          password  : config.password,
+          host      : config.host,
+          port      : config.port,
+          reconnect : true
+        })
+      }
 
       simplexmpp.send(config.to, title + ': ' + message)
-
-      simplexmpp.disconnect()
     }
   }
   return xmpp
