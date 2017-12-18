a393830e17e5200e55209924bdc84c8f1f8005f3
Johnathan James <haxwell@gmail.com>
2017-12-17 19:10:51 -0700

    Made use of timebucket in calculating current_days_left, for consistency.

--- commands/backfill.js
+++ commands/backfill.js
@@ -154,7 +154,7 @@ module.exports = function container (get, set, clear) {
                     if (err) throw err
                     trade_counter += trades.length
                     day_trade_counter += trades.length
-                    var current_days_left = Math.ceil((mode === 'backward' ? marker.oldest_time - target_time : target_time - marker.newest_time) / 86400000)
+                    var current_days_left = 1 + (mode === 'backward' ? tb(marker.oldest_time - target_time).resize('1d').value : tb(target_time - marker.newest_time).resize('1d').value)
                     if (current_days_left >= 0 && current_days_left != days_left) {
                       console.log('\n' + selector, 'saved', day_trade_counter, 'trades', current_days_left, 'days left')
                       day_trade_counter = 0
@@ -175,6 +175,7 @@ module.exports = function container (get, set, clear) {
                   })
                 })
               }
+
               runTasks()
             })
           }
