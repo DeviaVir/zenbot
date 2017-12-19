90870a75f8617ddf510c5cdc75e3f0914bc38a1d
Johnathan James <haxwell@gmail.com>
2017-12-19 09:50:50 -0700

    rename so period to so period size (#926)
    * Made use of timebucket in calculating current_days_left, for consistency.
    
    * Renamed so.period to so.periodSize. This more accurately describes what is stored in the variable. Especially because we use 'period' to describe a set of high and low values over time in engine.js. (See s.period in engine.js)

--- commands/sim.js
+++ commands/sim.js
@@ -48,6 +48,9 @@ module.exports = function container (get, set, clear) {
             so[k] = cmd[k]
           }
         })
+
+        so.periodSize = so.period
+
         if (so.start) {
           so.start = moment(so.start, "YYYYMMDDhhmm").valueOf()
           if (so.days && !so.end) {
@@ -78,7 +81,7 @@ module.exports = function container (get, set, clear) {
         var engine = get('lib.engine')(s)
         if (!so.min_periods) so.min_periods = 1
         var cursor, reversing, reverse_point
-        var query_start = so.start ? tb(so.start).resize(so.period).subtract(so.min_periods + 2).toMilliseconds() : null
+        var query_start = so.start ? tb(so.start).resize(so.periodSize).subtract(so.min_periods + 2).toMilliseconds() : null
 
         function exitSim () {
           console.log()
