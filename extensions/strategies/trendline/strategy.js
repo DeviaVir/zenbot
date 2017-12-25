e94b6cc86f8b58dc450cd73daeaa90acf48a6666
darthyvader <34537029+darthyvader@users.noreply.github.com>
2017-12-25 05:44:23 -0600

    for 30s period & arbitrage coins multi instance

--- extensions/strategies/trendline/strategy.js
+++ extensions/strategies/trendline/strategy.js
@@ -10,12 +10,12 @@ module.exports = function container (get, set, clear) {
     description: 'Calculate a trendline and trade when trend is positive vs negative.',
 
     getOptions: function () {
-      this.option('period', 'period length', String, '1s')
+      this.option('period', 'period length', String, '30s')
       this.option('lastpoints', "Number of trades for short trend average", Number, 100)
       this.option('avgpoints', "Number of trades for long trend average", Number, 1000)
       this.option('lastpoints2', "Number of trades for short trend average", Number, 10)
       this.option('avgpoints2', "Number of trades for long trend average", Number, 100)
-      this.option('min_periods', "Basically avgpoints + a BUNCH of more preroll periods for anything less than 5s period", Number, 50000)
+      this.option('min_periods', "Basically avgpoints + a BUNCH of more preroll periods for anything less than 5s period", Number, 15000)
       this.option('markup_sell_pct', "test", Number, 0)
       this.option('markdown_buy_pct', "test", Number, 0)
     },
