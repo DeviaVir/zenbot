418e77b44c0c9274155ac2e6d4cd7308c03dbb17
nedievas <nedievas@outlook.com>
2017-12-27 13:10:00 +0200

    CEXIO renew update-producs.sh (#980)
    via ccxt

--- extensions/exchanges/cexio/update-products.sh
+++ extensions/exchanges/cexio/update-products.sh
@@ -1,27 +1,20 @@
 #!/usr/bin/env node
-var request = require('micro-request')
-request('https://cex.io/api/currency_limits', {headers: {'User-Agent': 'zenbot/4'}}, function (err, resp, body) {
-  if (err) throw err
-  if (resp.statusCode !== 200) {
-    var err = new Error('non-200 status: ' + resp.statusCode)
-    err.code = 'HTTP_STATUS'
-    err.body = body
-    console.error(err)
-    process.exit(1)
-  }
+const ccxt = require('ccxt')
+const path = require('path')
+var client = new ccxt.cex()
+client.fetchMarkets().then(result => {
   var products = []
-  var pairs = body.data.pairs
-  pairs.forEach(function (product) {
+  result.forEach(function (product) {
     products.push({
-      asset: product.symbol1,
-      currency: product.symbol2,
-      min_size: product.minLotSize.toString(),
-      max_size: product.maxLotSize != null ? product.maxLotSize.toString() : product.maxLotSize,
-      increment: '0.00000001',
-      label: product.symbol1 + '/' + product.symbol2
+      asset: product.info.symbol1,
+      currency: product.info.symbol2,
+      min_size: product.info.minLotSize.toString(),
+      max_size: product.info.maxLotSize != null ? product.info.maxLotSize.toString() : product.info.maxLotSize,
+      increment: product.info.symbol1 === 'BTC' ? '0.01' : (product.info.symbol2 === 'BTC' ? '0.00000001' : '0.0001'),
+      label: product.id
     })
   })
-  var target = require('path').resolve(__dirname, 'products.json')
+  var target = path.resolve(__dirname, 'products.json')
   require('fs').writeFileSync(target, JSON.stringify(products, null, 2))
   console.log('wrote', target)
   process.exit()
