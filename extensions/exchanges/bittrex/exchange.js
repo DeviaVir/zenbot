6600afaf881239e6bd9d070e22ebc467b32f6936
Brice Messeca <bmesseca@gmail.com>
2018-01-03 14:29:56 +0100

    rename periodLength to period_length

--- extensions/exchanges/bittrex/exchange.js
+++ extensions/exchanges/bittrex/exchange.js
@@ -64,7 +64,7 @@ module.exports = function container(get, set, clear) {
       bittrex_public.getmarkethistory(args, function( data ) {
         if (!shownWarning) {
           console.log('please note: the bittrex api does not support backfilling (trade/paper only).')
-          console.log('please note: make sure to set the --periodLength=1m to make sure data for trade/paper is fetched.')
+          console.log('please note: make sure to set the --period_length=1m to make sure data for trade/paper is fetched.')
           shownWarning = true
         }
         if (typeof data !== 'object') {
--- extensions/strategies/noop/strategy.js
+++ extensions/strategies/noop/strategy.js
@@ -4,8 +4,8 @@ module.exports = function container (get, set, clear) {
     description: 'Just do nothing. Can be used to e.g. generate candlesticks for training the genetic forex strategy.',
 
     getOptions: function () {
-      this.option('period', 'period length, same as --periodLength', String, '30m')
-      this.option('periodLength', 'period length, same as --period', String, '30m')
+      this.option('period', 'period length, same as --period_length', String, '30m')
+      this.option('period_length', 'period length, same as --period', String, '30m')
     },
 
     calculate: function (s) {
