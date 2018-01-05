40320965fc09930cb6df6f95552099b1b5b4a627
T-rav <tmfrisinger@gmail.com>
2018-01-05 13:35:29 +0200

    Removed all ; from test file, seems as though the standard is no ;

--- test/lib/engine.test.js
+++ test/lib/engine.test.js
@@ -3,71 +3,71 @@ describe('Engine', function() {
 		it('when maker with buy_max_amt less than buy_pct amount should use buy_pct', function(){
 			
 			// arrange
-			var signal_type = 'buy';
-			var currency_amount = 1;
-			var buy_pct = 50;
-			var buy_max_amt = 0.25;			
-			var buySpy = jasmine.createSpy();
-			var sut = createEngine(currency_amount, buy_pct, buy_max_amt, buySpy);
+			var signal_type = 'buy'
+			var currency_amount = 1
+			var buy_pct = 50
+			var buy_max_amt = 0.25			
+			var buySpy = jasmine.createSpy()
+			var sut = createEngine(currency_amount, buy_pct, buy_max_amt, buySpy)
 			
 			// act
-			sut.executeSignal(signal_type);
+			sut.executeSignal(signal_type)
 			// assert
-			var expected = "2.77500000";
+			var expected = "2.77500000"
 			var buyArgs = buySpy.calls.mostRecent().args[0]
-			expect(buyArgs.size).toBe(expected);
-		});
+			expect(buyArgs.size).toBe(expected)
+		})
 		
 		it('when maker with buy_max_amt more than buy_pct amount should use buy_pct', function(){
 			// arrange
-			var signal_type = 'buy';
-			var currency_amount = 1;
-			var buy_pct = 50;
-			var buy_max_amt = 0.75;			
-			var buySpy = jasmine.createSpy();
-			var sut = createEngine(currency_amount, buy_pct, buy_max_amt, buySpy);
+			var signal_type = 'buy'
+			var currency_amount = 1
+			var buy_pct = 50
+			var buy_max_amt = 0.75			
+			var buySpy = jasmine.createSpy()
+			var sut = createEngine(currency_amount, buy_pct, buy_max_amt, buySpy)
 			
 			// act
-			sut.executeSignal(signal_type);
+			sut.executeSignal(signal_type)
 			// assert
-			var expected = "5.55000000";
+			var expected = "5.55000000"
 			var buyArgs = buySpy.calls.mostRecent().args[0]
-			expect(buyArgs.size).toBe(expected);
-		});
+			expect(buyArgs.size).toBe(expected)
+		})
 		
 		it('when maker with buy_max_amt equals buy_pct amount should use buy_pct', function(){
 			// arrange
-			var signal_type = 'buy';
-			var currency_amount = 1;
-			var buy_pct = 50;
-			var buy_max_amt = 0.50;			
-			var buySpy = jasmine.createSpy();
-			var sut = createEngine(currency_amount, buy_pct, buy_max_amt, buySpy);
+			var signal_type = 'buy'
+			var currency_amount = 1
+			var buy_pct = 50
+			var buy_max_amt = 0.50			
+			var buySpy = jasmine.createSpy()
+			var sut = createEngine(currency_amount, buy_pct, buy_max_amt, buySpy)
 			
 			// act
-			sut.executeSignal(signal_type);
+			sut.executeSignal(signal_type)
 			// assert
-			var expected = "5.55000000";
+			var expected = "5.55000000"
 			var buyArgs = buySpy.calls.mostRecent().args[0]
-			expect(buyArgs.size).toBe(expected);
-		});
+			expect(buyArgs.size).toBe(expected)
+		})
 	})
-});
+})
 
 function createEngine(currency_amount, buy_pct, buy_max_amt, buySpy){	
-	var fake_asset = "test_asset";
-	var fake_currency = "BTC";
-	var fake_exchange = "test_exchange";
-	var fake_project = "test_product";
-	var fake_bid = 0.10;
-	var fake_ask = 0.11;
-	var fake_balance = { currency: currency_amount, asset:0};
+	var fake_asset = "test_asset"
+	var fake_currency = "BTC"
+	var fake_exchange = "test_exchange"
+	var fake_project = "test_product"
+	var fake_bid = 0.10
+	var fake_ask = 0.11
+	var fake_balance = { currency: currency_amount, asset:0}
 	
 	var fakes = {
 		get: function() { },
 		set: function() { },
 		clear: function() { }
-	};
+	}
 	
 	var fake_product = {
 		"asset": fake_asset,
@@ -76,14 +76,14 @@ function createEngine(currency_amount, buy_pct, buy_max_amt, buySpy){
 		"max_size": null,
 		"increment": "0.01",
 		"label": "Test TST/BTC"
-	  };
+	  }
 
 	var fake_return = {
 		'conf': {},
 		'exchanges.test_exchange' : {
-			getProducts: function() { return [fake_product]; },
+			getProducts: function() { return [fake_product] },
 			getQuote: function(product, callback){ callback(null, { bid: fake_bid, ask: fake_ask}) },
-			getBalance: function(args, callback){ return callback(null, fake_balance);},
+			getBalance: function(args, callback){ return callback(null, fake_balance)},
 			buy: buySpy,
 			name: fake_exchange,
 			makerFee: 0.1,
@@ -96,9 +96,9 @@ function createEngine(currency_amount, buy_pct, buy_max_amt, buySpy){
 	
 	spyOn(fakes,"get").and.callFake(function(param){
 		return fake_return[param]
-	});
+	})
 	
-	var engine = require('../../lib/engine')(fakes.get, fakes.set, fakes.clear);
+	var engine = require('../../lib/engine')(fakes.get, fakes.set, fakes.clear)
 	var input = {
 		options: {
 			selector: {
@@ -114,6 +114,6 @@ function createEngine(currency_amount, buy_pct, buy_max_amt, buySpy){
 			buy_pct:buy_pct,
 			buy_max_amt:buy_max_amt
 		}
-	};
-	return engine(input);
+	}
+	return engine(input)
 }
