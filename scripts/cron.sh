51e413b0777250841a02e2bcea059df7b5746b83
Chase <chase@sillevis.net>
2018-01-15 10:31:38 +0100

    Travis: set up cron (#1139)

---
+++ scripts/cron.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+run_cron() {
+  git checkout unstable
+  git reset --hard origin/unstable
+  npm install -g node-gyp
+  npm install
+  for f in extensions/exchanges/*/update-products.sh;
+  do
+    echo "processing ${f}"
+    ./${f}
+  done
+
+  git add .
+  git commit --message "Exchanges: update-products $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git remote add upstream https://${GH_TOKEN}@github.com/DeviaVir/zenbot.git > /dev/null 2>&1
+  git push --quiet --set-upstream upstream unstable
+}
+
+setup_git
+run_cron
+upload_files
