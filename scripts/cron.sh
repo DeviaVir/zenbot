a3e50556ab829c9c2e5e99da35a8dce99b1192b9
Chase Sillevis <chase@sillevis.net>
2018-01-15 10:55:22 +0100

    Travis: run only on node 9

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 
+verify_environment() {
+  if [[ $(node -v | grep -q "v9" ; echo $?) -gt 0 ]];
+  then
+    echo "not node 9"
+    exit 0
+  fi
+}
+
 setup_git() {
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
+  git remote add upstream https://${GH_TOKEN}@github.com/DeviaVir/zenbot.git
+  git checkout unstable
+  git pull upstream unstable
 }
 
 run_cron() {
@@ -11,16 +22,15 @@ run_cron() {
     echo "processing ${f}"
     ./${f}
   done
-
-  git add .
-  git commit --message "Exchanges: update-products $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
-  git remote add upstream https://${GH_TOKEN}@github.com/DeviaVir/zenbot.git > /dev/null 2>&1
-  git push --quiet --set-upstream upstream unstable
+  git add .
+  git commit --message "Exchanges: update-products $TRAVIS_BUILD_NUMBER"
+  git push upstream unstable
 }
 
+verify_environment
 setup_git
 run_cron
 upload_files
