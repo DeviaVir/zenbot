e503800a08bacb7715612f6c9d26e41b44db9679
Michael Sutton <station384@gmail.com>
2018-02-14 04:20:47 -0500

    remove bad code (#1339)
    hoop now does nothing instead of throwing an error.

--- extensions/strategies/noop/strategy.js
+++ extensions/strategies/noop/strategy.js
@@ -8,23 +8,6 @@ module.exports = {
   },
 
   calculate: function () {
-
-      // Get the first bid/ask order amounts and price
-      s.exchange.getOrderBook({product_id: s.product_id}, function (err, order_quote) {
-        if (err) {
-          console.log('Error getting orderbook.');
-          throw err;
-        };
-        // Here we set the current Bid and Ask amount
-        s.options.myBidAmount = order_quote.buyOrderAmount;
-        s.options.myAskAmount = order_quote.sellOrderAmount;
-        // Here we set the current Bid and Ask amount of the first position in the order book
-        s.options.theBidPrice = order_quote.buyOrderRate;
-        s.options.theBidPrice = parseFloat(s.options.theBidPrice);
-        s.options.theAskPrice = order_quote.sellOrderRate;
-        s.options.theAskPrice = parseFloat(s.options.theAskPrice);
-      }
-
   },
 
   onPeriod: function (s, cb) {
