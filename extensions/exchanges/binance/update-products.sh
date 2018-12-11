d04c10321253eaf3e5e388743e1952ed0e42c184
Kévin Bernard-Allies <kevin.bernard.allies@gmail.com>
2018-12-11 12:23:34 +0100

    Fix binance product update (#1795)
    * Engine: Split code about check of minimal order size into a function
    
    The function avoid code duplication and is more easily readable.
    
    Small behavior change: the new function accept products with both `min_size` and `min_total`.
    Previously, only the first one available was checked.
    
    * Binance: rewrite update-products to fix bugs
    
    Changes with the previous version:
    - Binance filters are selected by type rather than by index. It fixes #1794
    - No more useless removal of trailing zeros of 'increment' and 'asset_increment'
    - Binance notional filter is set in `product.min_total` rather than roughly modifying `product.min_size`.
      It fixes #1793

--- extensions/exchanges/binance/update-products.sh
+++ extensions/exchanges/binance/update-products.sh
@@ -4,44 +4,24 @@ let ccxt = require('ccxt')
 new ccxt.binance().fetch_markets().then(function(markets) {
   var products = []
 
-  markets.forEach(function (market) {
-    var currStepSize = market.info.filters[0].tickSize
-    for (i = currStepSize.length - 1; i > 0; i--) {
-      if (currStepSize[i] === '0')
-        currStepSize = currStepSize.slice(0, i)
-      else
-        break;
-    }
+  var products = markets.map(function (market) {
+    const filters = market.info.filters
+    const price_filter = filters.find(f => f.filterType === 'PRICE_FILTER')
+    const lot_size_filter = filters.find(f => f.filterType === 'LOT_SIZE')
+    const notional_filter = filters.find(f => f.filterType === 'MIN_NOTIONAL')
 
-    var assetStepSize = market.info.filters[1].stepSize
-    for (i = assetStepSize.length - 1; i > 0; i--) {
-      if (assetStepSize[i] === '0')
-        assetStepSize = assetStepSize.slice(0, i)
-      else
-        break
-    }
-
-    // The MIN_NOTIONAL filter defines the minimum notional value allowed for an order on a symbol.
-    // An orders notional value is the price * quantity.
-    // In order to know the Value it is necessary to know the price which does not come on this JSon.
-    // But I have the maxPrice which seems to be: price * 10
-    var maxPrice       = Number(market.info.filters[0].maxPrice);
-    var curPrice       = maxPrice / 10;
-    var minNotional    = Number(market.info.filters[2].minNotional);
-    var minNotionalQty = minNotional / curPrice;
-    var minQty         = Number(market.info.filters[1].minQty);
-    var min_size       = Math.max(minQty, minNotionalQty).toString();
-
-    products.push({
+    // NOTE: price_filter also contains minPrice and maxPrice
+    return {
       id: market.id,
       asset: market.base,
       currency: market.quote,
-      min_size: min_size,
-      max_size: market.info.filters[1].maxQty,
-      increment: currStepSize,
-      asset_increment: assetStepSize,
+      min_size: lot_size_filter.minQty,
+      max_size: lot_size_filter.maxQty,
+      min_total: notional_filter.minNotional,
+      increment: price_filter.tickSize,
+      asset_increment: lot_size_filter.stepSize,
       label: market.base + '/' + market.quote
-    })
+    }
   })
 
   var target = require('path').resolve(__dirname, 'products.json')
