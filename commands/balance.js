5fe84b18dc2701640613bcf129fbe46435cb7ff5
Kévin Bernard-Allies <kevin@ebak.fr>
2019-01-23 18:21:01 +0100

    Fixes the balance command
    It's a quick commit aiming to fix immediate problems.
    The code is still far from perfect and should be modified in deep.
    
    Closes #1619

--- commands/balance.js
+++ commands/balance.js
@@ -3,7 +3,6 @@ var minimist = require('minimist')
   // eslint-disable-next-line no-unused-vars
   , colors = require('colors')
   , moment = require('moment')
-  , objectifySelector = require('../lib/objectify-selector')
   , exchangeService = require('../lib/services/exchange-service')
   , { formatCurrency } = require('../lib/format')
 
@@ -16,11 +15,20 @@ module.exports = function (program, conf) {
     .option('-c, --calculate_currency <calculate_currency>', 'show the full balance in another currency')
     .option('--debug', 'output detailed debug info')
     .action(function (selector, cmd) {
-      var s = {options: minimist(process.argv)}
-      s.selector = objectifySelector(selector || conf.selector)
-      s.product_id = s.selector.product_id
-      s.asset = s.selector.asset
-      s.currency = s.selector.currency
+
+      if (selector !== undefined)
+        conf.selector = selector
+
+      var exchangeServiceInstance = exchangeService(conf)
+      selector = exchangeServiceInstance.getSelector()
+
+      var s = {
+        options: minimist(process.argv),
+        selector: selector,
+        product_id: selector.product_id,
+        asset: selector.asset,
+        currency: selector.currency
+      }
 
       var so = s.options
       delete so._
@@ -34,12 +42,10 @@ module.exports = function (program, conf) {
       so.debug = cmd.debug
       so.mode = 'live'
       function balance () {
-        var exchangeServiceInstance = exchangeService(conf)
         var exchange = exchangeServiceInstance.getExchange()
 
-        var exchangeName = exchange.name // TODO: Refactor all exchanges to be in the format of the stub.exchange, so we can use getName() here.
         if (exchange === undefined) {
-          console.error('\nSorry, couldn\'t find an exchange named [' + exchangeName + '].')
+          console.error('\nSorry, couldn\'t find an exchange from selector [' + conf.selector + '].')
           process.exit(1)
         }
 
@@ -47,14 +53,14 @@ module.exports = function (program, conf) {
           if (err) throw err
           exchange.getQuote(s, function (err, quote) {
             if (err) throw err
-            
+
             var bal = moment().format('YYYY-MM-DD HH:mm:ss').grey + ' ' + formatCurrency(quote.ask, s.currency, true, true, false) + ' ' + (s.product_id).grey + '\n'
-            bal += moment().format('YYYY-MM-DD HH:mm:ss').grey + ' Asset: '.grey + balance.asset.white + ' Available: '.grey + n(balance.asset).subtract(balance.asset_hold).value().toString().yellow + '\n'
+            bal += moment().format('YYYY-MM-DD HH:mm:ss').grey + ' Asset: '.grey + n(balance.asset).format('0.00000000').white + ' Available: '.grey + n(balance.asset).subtract(balance.asset_hold).value().toString().yellow + '\n'
             bal += moment().format('YYYY-MM-DD HH:mm:ss').grey + ' Asset Value: '.grey + n(balance.asset).multiply(quote.ask).value().toString().white + '\n'
-            bal += moment().format('YYYY-MM-DD HH:mm:ss').grey + ' Currency: '.grey + balance.currency.white + ' Available: '.grey + n(balance.currency).subtract(balance.currency_hold).value().toString().yellow + '\n'
+            bal += moment().format('YYYY-MM-DD HH:mm:ss').grey + ' Currency: '.grey + n(balance.currency).format('0.00000000').white + ' Available: '.grey + n(balance.currency).subtract(balance.currency_hold).value().toString().yellow + '\n'
             bal += moment().format('YYYY-MM-DD HH:mm:ss').grey + ' Total: '.grey + n(balance.asset).multiply(quote.ask).add(balance.currency).value().toString().white
             console.log(bal)
-            
+
             if (so.calculate_currency) {
               exchange.getQuote({'product_id': s.asset + '-' + so.calculate_currency}, function (err, asset_quote) {
                 if (err)  throw err
