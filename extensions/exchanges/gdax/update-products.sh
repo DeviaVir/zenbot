0886504538b5ce1d3319513ea6a699f49a34e6be
Diego <42118791+mmdiego@users.noreply.github.com>
2019-10-03 02:27:12 -0300

    Remove trailing zeros from gdax products.json file (#1928) (#1950)
    Also sorts the products' list alphabetically.

--- extensions/exchanges/gdax/update-products.sh
+++ extensions/exchanges/gdax/update-products.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 var request = require('micro-request')
-request('https://api.gdax.com/products', {headers: {'User-Agent': 'zenbot/4'}}, function (err, resp, body) {
+request('https://api.pro.coinbase.com/products', {headers: {'User-Agent': 'zenbot/4'}}, function (err, resp, body) {
   if (err) throw err
   if (resp.statusCode !== 200) {
     var err = new Error('non-200 status: ' + resp.statusCode)
@@ -11,16 +11,34 @@ request('https://api.gdax.com/products', {headers: {'User-Agent': 'zenbot/4'}}, 
   }
   var products = []
   body.forEach(function (product) {
+    let regex = /\.0*$|(\.\d*[1-9])0+$/
     products.push({
       asset: product.base_currency,
       currency: product.quote_currency,
-      min_size: product.base_min_size,
-      max_size: product.base_max_size,
-      increment: product.quote_increment,
-      asset_increment: product.base_increment,
+      //min_size: product.base_min_size,
+      //max_size: product.base_max_size,
+      //increment: product.quote_increment,
+      //asset_increment: product.base_increment,
+      min_size: Number(product.base_min_size).toFixed(10).replace(regex,'$1'),
+      max_size: Number(product.base_max_size).toFixed(10).replace(regex,'$1'),
+      increment: Number(product.quote_increment).toFixed(10).replace(regex,'$1'),
+      asset_increment: Number(product.base_increment).toFixed(10).replace(/\.0*$|(\.\d*[1-9])0+$/,'$1'),
       label: product.display_name
     })
   })
+  products.sort(function(a, b) {
+    var nameA = a.label.toUpperCase(); // ignore upper and lowercase
+    var nameB = b.label.toUpperCase(); // ignore upper and lowercase
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+
+    // names must be equal
+    return 0;
+  });
   var target = require('path').resolve(__dirname, 'products.json')
   require('fs').writeFileSync(target, JSON.stringify(products, null, 2))
   console.log('wrote', target)
