b9c4af01c9a07e3d02380a0c75fb9649cee13100
Diego <42118791+mmdiego@users.noreply.github.com>
2020-11-30 13:29:54 -0300

    [#1893][#1838] Disable quarantine feature by default (#2519)

--- commands/trade.js
+++ commands/trade.js
@@ -73,7 +73,7 @@ module.exports = function (program, conf) {
         so.interval_trade = 10
       }
       if (!so.quarentine_time) {
-        so.quarentine_time = 10
+        so.quarentine_time = 0
       }
       delete so._
       if (cmd.conf) {
@@ -98,7 +98,7 @@ module.exports = function (program, conf) {
         console.log(('--buy_max_amt is deprecated, use --deposit instead!\n').red)
         so.deposit = so.buy_max_amt
       }
-      so.selector = objectifySelector(selector || conf.selector)      
+      so.selector = objectifySelector(selector || conf.selector)
       var engine = engineFactory(s, conf)
       var collectionServiceInstance = collectionService(conf)
       if (!so.min_periods) so.min_periods = 1
