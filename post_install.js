152f37cc59d6c4830e895c3abaa242d51d172d30
JeremyR <fcjreed@gmail.com>
2021-01-21 17:19:12 -0500

    Fix webpack issues and UI

--- post_install.js
+++ post_install.js
@@ -1,6 +1,6 @@
 var shell = require('shelljs')
 
 console.log('bundling WebApp components')
-shell.exec('webpack -p')
+shell.exec('webpack --mode production')
 console.log('installing genetic_backtester components')
 shell.exec('(cd scripts/genetic_backtester/ && npm i)')
