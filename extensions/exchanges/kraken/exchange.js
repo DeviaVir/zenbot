0dccf5e4932c7c787643eeb2ee5577c98acc4ce1
Jeff Mixon <jeff.mixon@gmail.com>
2021-02-12 14:39:57 -0800

    fix: broken kraken products
    Removes the need for manual mapping due to Kraken's inconsistent naming
    conventions for asset pairs.
    
    Resolves #2632

--- extensions/exchanges/kraken/exchange.js
+++ extensions/exchanges/kraken/exchange.js
@@ -38,35 +38,6 @@ module.exports = function container(conf) {
   function joinProductFormatted(product_id) {
     var asset = product_id.split('-')[0]
     var currency = product_id.split('-')[1]
-
-    var fixMap = {
-      'XETH': 'ETH',
-      'XXBT': 'XBT',
-      'XLTC': 'LTC',
-      'XXDG': 'XDG',
-      'XETC': 'ETC',
-      'XMLN': 'MLN',
-      'XREP': 'REP',
-      'XXRP': 'XRP',
-      'XXLM': 'XLM',
-      'XXMR': 'XMR',
-      'XZEC': 'ZEC',
-      'ZAUD': 'AUD',
-      'ZEUR': 'EUR',
-      'ZGBP': 'GBP',
-      'ZUSD': 'USD',
-      'ZJPY': 'JPY',
-      'ZCAD': 'CAD',
-    }
-
-    if (asset in fixMap) {
-      asset = fixMap[asset]
-    }
-
-    if (currency in fixMap) {
-      currency = fixMap[currency]
-    }
-
     return asset + currency
   }
 
--- extensions/exchanges/kraken/update-products.sh
+++ extensions/exchanges/kraken/update-products.sh
@@ -3,47 +3,41 @@
 var KrakenClient = require('kraken-api')
 var kraken = new KrakenClient()
 
-var mapping
 var products = []
 
-function addProduct(base, quote, altname, min_size, increment) {
+function addProduct (base, quote, label, min_size, increment) {
   products.push({
     asset: base,
     currency: quote,
     min_size: parseFloat(min_size).toFixed(10),
     increment: (10 ** (-1 * increment)).toFixed(10),
-    label: getPair(base) + '/' + getPair(quote)
+    label: label
   })
 }
 
-function getPair(name) {
-  return mapping[name].altname
-}
-
-kraken.api('Assets', null, function(error, data) {
+kraken.api('AssetPairs', null, function (error, data) {
   if (error) {
     console.log(error)
     process.exit(1)
   } else {
+    Object.keys(data.result).forEach(function (pair) {
+      if (!pair.match('.d')) {
+        if (!data.result[pair].wsname) {
+          console.warn(`Cannot identify pair for ${pair}`)
+          return
+        }
+        const wsname = data.result[pair].wsname
+        const split = wsname.split('/')
+        const asset = pair.substring(0, pair.indexOf(split[0]) + split[0].length)
+        const currency = pair.substring(asset.length)
 
-    mapping = data.result
-
-    kraken.api('AssetPairs', null, function(error, data) {
-      if (error) {
-        console.log(error)
-        process.exit(1)
-      } else {
-        Object.keys(data.result).forEach(function(result) {
-          if (!result.match('\.d')) {
-            addProduct(data.result[result].base, data.result[result].quote, data.result[result].altname,
-                       data.result[result].ordermin, data.result[result].pair_decimals)
-          }
-        })
-        var target = require('path').resolve(__dirname, 'products.json')
-        require('fs').writeFileSync(target, JSON.stringify(products, null, 2))
-        console.log('wrote', target)
-        process.exit()
+        addProduct(asset, currency, wsname,
+          data.result[pair].ordermin, data.result[pair].pair_decimals)
       }
     })
+    var target = require('path').resolve(__dirname, 'products.json')
+    require('fs').writeFileSync(target, JSON.stringify(products, null, 2))
+    console.log('wrote', target)
+    process.exit()
   }
 })
